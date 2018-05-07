eeac868b858625c582f79f98c923b8d021aa46a8
Bram Oosterlee <bmoosterlee@gmail.com>
2018-05-07 15:59:42 +0200

    Create Harmonic class

---
+++ src/Harmonic.java
@@ -0,0 +1,11 @@
+public class Harmonic {
+
+    Note tonic;
+    Fraction harmonicAsFraction;
+
+    public Harmonic(Note tonic){
+        tonic = tonic;
+        harmonicAsFraction = new Fraction(1, 1);
+    }
+
+}
