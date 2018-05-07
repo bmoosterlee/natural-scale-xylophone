0ec87b49ff2b65c90930f88525dafb74a97bce1d
Bram Oosterlee <bmoosterlee@gmail.com>
2018-05-07 15:59:01 +0200

    Create Fraction class instead of using two paired integers

---
+++ src/Fraction.java
@@ -0,0 +1,11 @@
+public class Fraction {
+
+    int numerator;
+    int denominator;
+
+    public Fraction(int numerator, int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+}
