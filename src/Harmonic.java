a273084c6e1c40ff4b872568ab37b32182d00929
Bram Oosterlee <bmoosterlee@gmail.com>
2018-05-10 11:36:49 +0200

    Implement comparability between harmonics

--- src/Harmonic.java
+++ src/Harmonic.java
@@ -1,4 +1,4 @@
-public class Harmonic {
+public class Harmonic implements Comparable<Harmonic>{
 
     Note tonic;
     Fraction harmonicAsFraction;
@@ -8,4 +8,24 @@ public class Harmonic {
         harmonicAsFraction = new Fraction(1, 1);
     }
 
+    @Override
+    public int compareTo(Harmonic otherHarmonic) {
+        if(harmonicAsFraction.denominator<otherHarmonic.harmonicAsFraction.denominator) {
+            return -1;
+        }
+        else if(harmonicAsFraction.denominator==otherHarmonic.harmonicAsFraction.denominator){
+            if(harmonicAsFraction.numerator<otherHarmonic.harmonicAsFraction.numerator){
+                return -1;
+            }
+            else if(harmonicAsFraction.numerator==otherHarmonic.harmonicAsFraction.numerator){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+        else {
+            return 1;
+        }
+    }
 }
