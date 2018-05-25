2646400b163043f12a21ee977faeb875e7253bcb
Bram Oosterlee <bmoosterlee@gmail.com>
2018-05-25 17:25:35 +0200

    Rewrite Harmonic's compareTo method to use sonance

--- src/Harmonic.java
+++ src/Harmonic.java
@@ -2,6 +2,7 @@ public class Harmonic implements Comparable<Harmonic>{
 
     Note tonic;
     Fraction harmonicAsFraction;
+    long lastSampleCount;
 
     public Harmonic(Note tonic, Fraction harmonicAsFraction){
         this.tonic = tonic;
@@ -9,24 +10,8 @@ public class Harmonic implements Comparable<Harmonic>{
     }
 
     @Override
-    public int compareTo(Harmonic otherHarmonic) {
-        if(harmonicAsFraction.denominator<otherHarmonic.harmonicAsFraction.denominator) {
-            return -1;
-        }
-        else if(harmonicAsFraction.denominator==otherHarmonic.harmonicAsFraction.denominator){
-            if(harmonicAsFraction.numerator<otherHarmonic.harmonicAsFraction.numerator){
-                return -1;
-            }
-            else if(harmonicAsFraction.numerator==otherHarmonic.harmonicAsFraction.numerator){
-                return 0;
-            }
-            else{
-                return 1;
-            }
-        }
-        else {
-            return 1;
-        }
+    public int compareTo(Harmonic o) {
+        return Double.compare(getSonanceValue(lastSampleCount), o.getSonanceValue(o.lastSampleCount))*-1;
     }
 
     public double getSonanceValue(long sampleCount) {
