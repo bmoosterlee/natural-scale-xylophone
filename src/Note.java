46ccadd53a37067d4fd41b2e96e7a65202afe6af
Bram Oosterlee <bmoosterlee@gmail.com>
2018-05-25 18:35:07 +0200

    Rewrite Note to play notes for a fixed length, with a linear decay curve

--- src/Note.java
+++ src/Note.java
@@ -2,6 +2,7 @@ public class Note {
 
     private double frequency;
     private long startingSampleCount;
+    private final double noteLengthInSeconds = 3.;
 
     public Note(double frequency, long startingSampleCount){
         this.frequency = frequency;
@@ -9,16 +10,27 @@ public class Note {
     }
 
     public double getAmplitude(float sampleRate, long sampleCount) {
-        double volume = getVolume(sampleCount);
+        double volume = getVolume(sampleRate, sampleCount);
         long sampleCountDifference = sampleCount - getStartingSampleCount();
         double timeDifference = sampleCountDifference / sampleRate;
         double angle = timeDifference * getFrequency() * 2.0 * Math.PI;
         return (Math.sin(angle) * volume);
     }
 
-    public double getVolume(long sampleCount) {
+    public double getVolume(float sampleRate, long sampleCount) {
         //TODO Create method with decay as an argument, let harmonics extract the volume for a longer decay, such that they remain on the screen longer
-        return 0.5*10000./(10000.+(sampleCount- getStartingSampleCount()));
+        long sampleCountDifference = sampleCount - getStartingSampleCount();
+        double timeDifference = sampleCountDifference / sampleRate;
+
+        if(timeDifference<0){
+            return 0;
+        }
+        if(timeDifference>=noteLengthInSeconds){
+            return 0;
+        }
+        else {
+            return 1.0 - timeDifference / noteLengthInSeconds;
+        }
     }
 
     public double getFrequency() {
