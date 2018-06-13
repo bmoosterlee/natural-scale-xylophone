8c3eb7764a6ca90d9ae4dcf7541da10650fa3df3
Bram Oosterlee <bmoosterlee@gmail.com>
2018-06-13 16:24:22 +0200

    Delegate memoized fraction calculation to separate class

---
+++ src/FractionCalculator.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class FractionCalculator {
+    private ArrayList<Fraction> calculatedFractions;
+    private int lastCalculatedIndex;
+    private RelativelyPrimeFractionIterator iterator;
+
+    public FractionCalculator() {
+        setIterator(new RelativelyPrimeFractionIterator());
+        setCalculatedFractions(new ArrayList<>());
+        setLastCalculatedIndex(-1);
+    }
+
+    public ArrayList<Fraction> getCalculatedFractions() {
+        return calculatedFractions;
+    }
+
+    public void setCalculatedFractions(ArrayList<Fraction> calculatedFractions) {
+        this.calculatedFractions = calculatedFractions;
+    }
+
+    public int getLastCalculatedIndex() {
+        return lastCalculatedIndex;
+    }
+
+    public void setLastCalculatedIndex(int lastCalculatedIndex) {
+        this.lastCalculatedIndex = lastCalculatedIndex;
+    }
+
+    public RelativelyPrimeFractionIterator getIterator() {
+        return iterator;
+    }
+
+    public void setIterator(RelativelyPrimeFractionIterator iterator) {
+        this.iterator = iterator;
+    }
+
+    public Fraction getCalculatedFraction(int index){
+        return getCalculatedFractions().get(index);
+    }
+
+    public Fraction calculateNextFraction() {
+        Fraction newFraction  = getIterator().next();
+        getCalculatedFractions().add(newFraction);
+        setLastCalculatedIndex(getLastCalculatedIndex() + 1);
+        return newFraction;
+    }
+
+    public Fraction getFraction(int index) {
+        if(index > getLastCalculatedIndex()){
+            Fraction newFraction = calculateNextFraction();
+            return newFraction;
+        }
+        else{
+            return getCalculatedFraction(index);
+        }
+    }
+}
--- src/NoteHarmonicCalculator.java
+++ src/NoteHarmonicCalculator.java
@@ -2,20 +2,15 @@ import java.util.ArrayList;
 
 public class NoteHarmonicCalculator implements Comparable<NoteHarmonicCalculator>{
 
+    private final FractionCalculator fractionCalculator = new FractionCalculator();
     private Note note;
     private Fraction currentFraction;
     private int index;
-    private ArrayList<Fraction> calculatedFractions;
-    private int lastCalculatedIndex;
-    private RelativelyPrimeFractionIterator iterator;
     private double noteVolume;
 
     public NoteHarmonicCalculator(Note note, double noteVolume){
         this.setNote(note);
-        setIterator(new RelativelyPrimeFractionIterator());
-        setCalculatedFractions(new ArrayList<>());
         setIndex(0);
-        setLastCalculatedIndex(-1);
         setCurrentFraction(getNextHarmonic());
         this.setNoteVolume(noteVolume);
     }
@@ -30,15 +25,7 @@ public class NoteHarmonicCalculator implements Comparable<NoteHarmonicCalculator
     }
 
     private Fraction getNextHarmonic() {
-        if(getIndex() > getLastCalculatedIndex()){
-            Fraction newFraction  = getIterator().next();
-            getCalculatedFractions().add(newFraction);
-            setLastCalculatedIndex(getLastCalculatedIndex() + 1);
-            return newFraction;
-        }
-        else{
-            return getCalculatedFractions().get(getIndex());
-        }
+        return fractionCalculator.getFraction(getIndex());
     }
 
     public Harmonic peek(){
@@ -50,30 +37,6 @@ public class NoteHarmonicCalculator implements Comparable<NoteHarmonicCalculator
     @Override
     public int compareTo(NoteHarmonicCalculator o) {
         return peek().compareTo(o.peek());
-    }
-
-    public ArrayList<Fraction> getCalculatedFractions() {
-        return calculatedFractions;
-    }
-
-    public void setCalculatedFractions(ArrayList<Fraction> calculatedFractions) {
-        this.calculatedFractions = calculatedFractions;
-    }
-
-    public int getLastCalculatedIndex() {
-        return lastCalculatedIndex;
-    }
-
-    public void setLastCalculatedIndex(int lastCalculatedIndex) {
-        this.lastCalculatedIndex = lastCalculatedIndex;
-    }
-
-    public RelativelyPrimeFractionIterator getIterator() {
-        return iterator;
-    }
-
-    public void setIterator(RelativelyPrimeFractionIterator iterator) {
-        this.iterator = iterator;
     }
 
     public Note getNote() {
