5618628304a715ff2a9ec06003c58d27b7358cc6
Bram Oosterlee <bmoosterlee@gmail.com>
2018-06-14 13:54:35 +0200

    Let MemoizedIterator implement Iterator

--- src/MemoizedIterator.java
+++ src/MemoizedIterator.java
@@ -1,9 +1,15 @@
-public class MemoizedIterator {
+import java.util.Iterator;
+
+public class MemoizedIterator implements Iterator<Fraction> {
 
     final static FractionMemoizer FRACTION_MEMOIZER = new FractionMemoizer();
     int index;
 
-    Fraction next() {
+    public boolean hasNext(){
+        return true;
+    }
+
+    public Fraction next() {
         Fraction nextHarmonicAsFraction = FRACTION_MEMOIZER.getFraction(index);
         this.index = index + 1;
         return nextHarmonicAsFraction;
