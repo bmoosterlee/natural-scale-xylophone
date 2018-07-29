10856f18b388fab4a073a4ebbdf8458fe6e854eb
Bram Oosterlee <bmoosterlee@gmail.com>
2018-07-29 18:16:20 +0200

    Rename HarmonicBuffer variables

--- src/HarmonicBuffer.java
+++ src/HarmonicBuffer.java
@@ -3,33 +3,33 @@ import javafx.util.Pair;
 import java.util.*;
 
 public class HarmonicBuffer {
-    HashSet<Harmonic> previousHighHarmonics;
-    HashMap<Harmonic, Note> previousHighHarmonicNotes;
+    HashSet<Harmonic> bufferedHarmonics;
+    HashMap<Harmonic, Note> noteTable;
     PriorityQueue<Harmonic> harmonicHierarchy;
     HashMap<Harmonic, Double> volumeTable;
-    HashMap<Note, HashSet<Harmonic>> notesForPreviousHighHarmonics;
+    HashMap<Note, HashSet<Harmonic>> harmonicsTable;
 
     public HarmonicBuffer() {
-        previousHighHarmonics = new HashSet<>();
-        previousHighHarmonicNotes = new HashMap<>();
+        bufferedHarmonics = new HashSet<>();
+        noteTable = new HashMap<>();
         volumeTable = new HashMap<>();
 
-        notesForPreviousHighHarmonics = new HashMap<>();
+        harmonicsTable = new HashMap<>();
     }
 
     void addHarmonicToHarmonicBuffer(double newHarmonicVolume, Note highestValueNote, Harmonic highestValueHarmonic) {
         synchronized (this) {
-            previousHighHarmonics.add(highestValueHarmonic);
-            previousHighHarmonicNotes.put(highestValueHarmonic, highestValueNote);
+            bufferedHarmonics.add(highestValueHarmonic);
+            noteTable.put(highestValueHarmonic, highestValueNote);
             volumeTable.put(highestValueHarmonic, newHarmonicVolume);
             harmonicHierarchy.add(highestValueHarmonic);
 
-            if (!notesForPreviousHighHarmonics.containsKey(highestValueNote)) {
+            if (!harmonicsTable.containsKey(highestValueNote)) {
                 HashSet<Harmonic> harmonicsForThisNote = new HashSet<>();
                 harmonicsForThisNote.add(highestValueHarmonic);
-                notesForPreviousHighHarmonics.put(highestValueNote, harmonicsForThisNote);
+                harmonicsTable.put(highestValueNote, harmonicsForThisNote);
             } else {
-                notesForPreviousHighHarmonics.get(highestValueNote).add(highestValueHarmonic);
+                harmonicsTable.get(highestValueNote).add(highestValueHarmonic);
             }
         }
     }
@@ -48,9 +48,9 @@ public class HarmonicBuffer {
 //            store volumes in a pair with the harmonic in a priorityqueue
 
         PriorityQueue<Harmonic> newHarmonicPriorityQueue = new PriorityQueue<>((o1, o2) -> -volumeTable.get(o1).compareTo(volumeTable.get(o2)));
-        synchronized (notesForPreviousHighHarmonics) {
+        synchronized (harmonicsTable) {
             for(Map.Entry<Note,Double> pair : new HashSet<>(noteVolumeTable.entrySet())){
-                HashSet<Harmonic> harmonics = notesForPreviousHighHarmonics.get(pair.getKey());
+                HashSet<Harmonic> harmonics = harmonicsTable.get(pair.getKey());
                 if(harmonics!=null) {
                     for (Harmonic harmonic : harmonics) {
                         newHarmonicPriorityQueue.add(harmonic);
@@ -68,21 +68,21 @@ public class HarmonicBuffer {
     void removeNote(Note note) {
         synchronized(this) {
             //            remove dead notes here based on volume
-            HashSet<Harmonic> harmonics = notesForPreviousHighHarmonics.get(note);
+            HashSet<Harmonic> harmonics = harmonicsTable.get(note);
             if(harmonics!=null) {
                 for (Harmonic harmonic : harmonics) {
-                    previousHighHarmonics.remove(harmonic);
-                    previousHighHarmonicNotes.remove(harmonic);
+                    bufferedHarmonics.remove(harmonic);
+                    noteTable.remove(harmonic);
                 }
             }
 
-            notesForPreviousHighHarmonics.remove(note);
+            harmonicsTable.remove(note);
         }
     }
 
     void addNote(Note note) {
-        synchronized(notesForPreviousHighHarmonics) {
-            notesForPreviousHighHarmonics.put(note, new HashSet<>());
+        synchronized(harmonicsTable) {
+            harmonicsTable.put(note, new HashSet<>());
         }
     }
 
