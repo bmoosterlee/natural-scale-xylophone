34cb9e1822f299781c9061b15c2896e6d7137359
bramo <bmoosterlee@gmail.com>
2018-08-01 01:57:06 +0200

    Refactor Buckets internals

--- src/Buckets.java
+++ src/Buckets.java
@@ -1,28 +1,26 @@
 
 public class Buckets {
-    private double[] buckets;
-    private final int width;
+    private double[] bucketsData;
 
     public Buckets(int width) {
-        this.width = width;
-        buckets = new double[width];
+        bucketsData = new double[width];
     }
 
     void clear() {
-        for(int i = 0; i< width; i++){
-            buckets[i] = 0;
+        for(int i = 0; i< bucketsData.length; i++){
+            bucketsData[i] = 0;
         }
     }
 
     void fill(int x, double value) {
-        buckets[x]+=value;
+        bucketsData[x]+=value;
     }
 
     void put(int x, double value) {
-        buckets[x] = value;
+        bucketsData[x] = value;
     }
 
     double getValue(int i) {
-        return buckets[i];
+        return bucketsData[i];
     }
 }
