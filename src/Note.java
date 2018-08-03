170b37919231c8df4d365ee100f063767b9d8380
bramo <bmoosterlee@gmail.com>
2018-08-03 16:44:11 +0200

    Extract constant value frequencyPi

--- src/Note.java
+++ src/Note.java
@@ -2,16 +2,18 @@ public class Note {
 
     private final Envelope envelope;
     private double frequency;
+    private double frequencyPi;
 
     public Note(double frequency, long startingSampleCount, float sampleRate){
-        this.frequency = frequency;
         this.envelope = new Envelope(startingSampleCount, sampleRate);
+        this.frequency = frequency;
+        this.frequencyPi = getFrequency() * 2.0 * Math.PI;
     }
 
     public double getAmplitude(long sampleCount) {
         double volume = getEnvelope().getVolume(sampleCount);
         double timeDifference = getEnvelope().getTimeDifference(sampleCount);
-        double angle = timeDifference * getFrequency() * 2.0 * Math.PI;
+        double angle = timeDifference * frequencyPi;
         return (Math.sin(angle) * volume);
     }
 
