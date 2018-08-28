356ed22eb1cd1804795c45d636600ed6b6dbfc22
bramo <bmoosterlee@gmail.com>
2018-08-28 21:36:12 +0200

    Fix getVolume internals

--- src/Envelope.java
+++ src/Envelope.java
@@ -11,8 +11,8 @@ public class Envelope {
     }
 
     public double getVolume(long sampleCount) {
-        // return getVolumeAsymptotic(sampleCount, amplitude, 1.5);
-        return getVolumeLinear(sampleCount, amplitude, 50.0);
+        // return getVolumeAsymptotic(sampleCount, amplitude, 10);
+        return getVolumeLinear(sampleCount, amplitude, 1.0);
     }
 
     double getVolumeLinear(long sampleCount, double amplitude, double noteLengthInSeconds) {
@@ -23,18 +23,25 @@ public class Envelope {
         }
         if (timeDifference >= noteLengthInSeconds) {
             return 0;
-        } else {
-            return amplitude - timeDifference / noteLengthInSeconds;
+        }
+        else {
+            double attack = 0.1;
+            if(timeDifference < noteLengthInSeconds*attack){
+                return amplitude * timeDifference / (noteLengthInSeconds*attack);
+            }
+            else {
+                return amplitude * (1 - (timeDifference-noteLengthInSeconds*attack) / (noteLengthInSeconds*(1-attack)));
+            }
         }
     }
 
-    double getVolumeAsymptotic(long sampleCount, double amplitude, double exponent) {
+    double getVolumeAsymptotic(long sampleCount, double amplitude, double multiplier) {
         double timeDifference = getTimeDifference(sampleCount);
 
         if (timeDifference < 0) {
             return 0;
         } else {
-            return amplitude / (Math.pow(timeDifference, exponent) + 1);
+            return amplitude / (timeDifference*multiplier + 1);
         }
     }
 
