df50ff0e1c9c182c169aabd1f4554c55809a8ae2
bramo <bmoosterlee@gmail.com>
2018-09-17 22:35:46 +0200

    Remove finished todo

--- src/notes/state/NoteState.java
+++ src/notes/state/NoteState.java
@@ -6,9 +6,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class NoteState {
-    //todo write a construction which makes sure notes is updated and removes dead deterministic notes
-    //todo we could use sampleCount in getSnapshot, and return the updated list. The only issue is that new notes won't
-    //todo be added yet.
     public final Set<Note> notes;
 
     public NoteState(){
