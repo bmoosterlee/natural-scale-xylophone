5501f32e8904b0111ad493487d0c062113b2e5a0
Bram Oosterlee <bmoosterlee@gmail.com>
2018-10-08 01:08:57 +0200

    Implement PatternPauser PianolaPattern

---
+++ src/pianola/PatternPauser.java
@@ -0,0 +1,33 @@
+package pianola;
+
+import frequency.Frequency;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PatternPauser implements PianolaPattern {
+    PianolaPattern pianolaPattern;
+    int patternSize;
+    Sequencer sequencer;
+
+    PatternPauser(int size, PianolaPattern pianolaPattern, int patternSize){
+        this.pianolaPattern = pianolaPattern;
+        this.patternSize = patternSize;
+        sequencer = new Sequencer(size, 1);
+    }
+
+    @Override
+    public Set<Frequency> playPattern() {
+        Set<Frequency> frequencies;
+
+        if(sequencer.i<patternSize){
+            frequencies = pianolaPattern.playPattern();
+        }
+        else{
+            frequencies = new HashSet<>();
+        }
+
+        sequencer.tick();
+        return frequencies;
+    }
+}
