8e022e9ebc1cf2aa62040ff8e2913225ff28991e
Bram Oosterlee <bmoosterlee@gmail.com>
2018-10-07 06:19:48 +0200

    Remove finished todos

--- src/frequency/SimpleFrequencyState.java
+++ src/frequency/SimpleFrequencyState.java
@@ -103,11 +103,6 @@ public class SimpleFrequencyState<T extends FrequencyStateBucket> implements Fre
             newFrequencies.add(frequency);
         }
 
-        //todo if we want the list to be immutable, we could calculate the death time of each note, and create a
-        //todo new version of the object at each death time.
-        //todo by using zipper like structures, we can only update a particular note list.
-        //todo we can link together all these death time based frequencytables, and request the current one from the
-        // todo noteManager, like an iterator. We might even turn it into an iterator.
         Map<Frequency, T> newBuckets = new HashMap<>(buckets);
         T newBucket;
         try {
--- src/notes/Note.java
+++ src/notes/Note.java
@@ -3,15 +3,6 @@ package notes;
 import frequency.Frequency;
 import notes.envelope.Envelope;
 
-    //todo create a wave class which can precalculate amplutides.
-    //todo we give the wave class a starting sample count, and an end sample count or timeframe in samples
-    //todo the wave will then precalculate it's amplitudes for these samples
-
-    //todo by default, wave only have an amplitude calculation function.
-    //todo the precalculated wave will be built on top of the wave structure, and have a start and end time.
-    //todo if we use immutable data structures to store the wave data, we can easily build a new precalc wave
-    //todo on top of an old one, by simply copying the data structure and adding onto it, by using a zipper for example.
-
 public class Note<T extends Envelope> {
     private final Frequency frequency;
     private final T envelope;
