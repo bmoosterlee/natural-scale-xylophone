8dd5813c62128cfd3754ccc357853afbe48b51c1
Bram Oosterlee <bmoosterlee@gmail.com>
2018-10-08 18:07:03 +0200

    Change modifiers

--- src/harmonics/CalculatorSnapshot.java
+++ src/harmonics/CalculatorSnapshot.java
@@ -4,17 +4,16 @@ import frequency.Frequency;
 
 import java.util.*;
 
-public class CalculatorSnapshot {
-    //todo change fields to final
-    private Map<Frequency, Double> volumeTable;
-    private PriorityQueue<Frequency> iteratorHierarchy;
+class CalculatorSnapshot {
+    private final Map<Frequency, Double> volumeTable;
+    private final PriorityQueue<Frequency> iteratorHierarchy;
 
-    public CalculatorSnapshot(Set<Frequency> liveFrequencies, Map<Frequency, MemoableIterator> iteratorTable, Map<Frequency, Double> volumeTable) {
+    CalculatorSnapshot(Set<Frequency> liveFrequencies, Map<Frequency, MemoableIterator> iteratorTable, Map<Frequency, Double> volumeTable) {
         this.volumeTable = volumeTable;
         this.iteratorHierarchy = buildIteratorHierarchy(this.volumeTable, liveFrequencies, iteratorTable);
     }
 
-    PriorityQueue<Frequency> buildIteratorHierarchy(Map<Frequency, Double> volumeTable, Set<Frequency> liveFrequencies, Map<Frequency, MemoableIterator> iteratorTable) {
+    private PriorityQueue<Frequency> buildIteratorHierarchy(Map<Frequency, Double> volumeTable, Set<Frequency> liveFrequencies, Map<Frequency, MemoableIterator> iteratorTable) {
         PriorityQueue<Frequency> iteratorHierarchy = new PriorityQueue<>((o1, o2) -> -Double.compare(
                 Harmonic.getHarmonicValue(volumeTable.get(o1), iteratorTable.get(o1).peek()),
                 Harmonic.getHarmonicValue(volumeTable.get(o2), iteratorTable.get(o2).peek()))
@@ -24,11 +23,11 @@ public class CalculatorSnapshot {
         return iteratorHierarchy;
     }
 
-    public Map<Frequency, Double> getVolumeTable() {
+    Map<Frequency, Double> getVolumeTable() {
         return volumeTable;
     }
 
-    public PriorityQueue<Frequency> getIteratorHierarchy() {
+    PriorityQueue<Frequency> getIteratorHierarchy() {
         return iteratorHierarchy;
     }
 
