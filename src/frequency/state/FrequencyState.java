8bc7c884a6fbb1b0b142f5ad8844323bdfbcc8b8
Bram Oosterlee <bmoosterlee@gmail.com>
2018-10-08 21:11:53 +0200

    Format code

--- src/frequency/state/FrequencyState.java
+++ src/frequency/state/FrequencyState.java
@@ -5,20 +5,25 @@ import notes.Note;
 
 import java.util.*;
 
+
+
+
+
+
 public interface FrequencyState {
-
-    FrequencyState addNote(Note note);
-
-    Set<Frequency> getFrequencies();
-
-    FrequencyState removeNote(Note note);
-
-    Map<Frequency, Double> getFrequencyVolumeTable(long sampleCount);
-
-    Double getVolume(Frequency frequency, long sampleCount);
 
     FrequencyState update(Set<Note> notes);
 
     FrequencyState update(long sampleCount);
+
+    FrequencyState addNote(Note note);
+
+    FrequencyState removeNote(Note note);
+
+    Set<Frequency> getFrequencies();
+
+    Double getVolume(Frequency frequency, long sampleCount);
+
+    Map<Frequency, Double> getFrequencyVolumeTable(long sampleCount);
 
 }
