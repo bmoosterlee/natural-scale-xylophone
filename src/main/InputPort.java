59b4084916bbd578afdc1841695728ba3ba963c6
Bram Oosterlee <bmoosterlee@gmail.com>
2018-10-13 22:03:04 +0200

    Implement flush method

--- src/main/InputPort.java
+++ src/main/InputPort.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class InputPort<T> {
 
     private final BoundedBuffer<T> buffer;
@@ -12,4 +15,15 @@ public class InputPort<T> {
         return buffer.poll();
     }
 
+    public List<T> flush() throws InterruptedException {
+        int length = buffer.filledSpots.availablePermits();
+        int count = 0;
+
+        List<T> list = new LinkedList<>();
+        while(buffer.filledSpots.availablePermits() != 0 && count<length){
+            list.add(buffer.poll());
+            count++;
+        }
+        return list;
+    }
 }
