8bd4901db74f3227c67661412064cab378b53df3
Bram Oosterlee <bmoosterlee@gmail.com>
2018-10-14 20:56:33 +0200

    Implement batch write

--- src/sound/SoundEnvironment.java
+++ src/sound/SoundEnvironment.java
@@ -9,6 +9,8 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SoundEnvironment implements Runnable{
     private final SampleRate sampleRate;
@@ -64,6 +66,32 @@ public class SoundEnvironment implements Runnable{
 
     private void writeToBuffer(byte amplitude) {
         sourceDataLine.write(new byte[]{amplitude}, 0, 1);
+    }
+
+    private void tickBatch() {
+        try {
+            List<Double> amplitudes = sampleAmplitudeInput.flushOrConsume();
+            int size = amplitudes.size();
+
+            TimeKeeper timeKeeper = PerformanceTracker.startTracking("fit amplitude");
+            int i = 0;
+            byte[] fittedAmplitudes = new byte[size];
+            for(Double amplitude : amplitudes) {
+                fittedAmplitudes[i++] = fitAmplitude(amplitude);
+            }
+            PerformanceTracker.stopTracking(timeKeeper);
+
+            timeKeeper = PerformanceTracker.startTracking("write to buffer");
+            writeToBuffer(fittedAmplitudes, size);
+            PerformanceTracker.stopTracking(timeKeeper);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeToBuffer(byte[] amplitudes, int size) {
+        sourceDataLine.write(amplitudes, 0, size);
     }
 
     private byte fitAmplitude(double amplitude) {
