12bfe6a9740ec91239312ff3e93732e382166e39
Bram Oosterlee <bmoosterlee@gmail.com>
2018-10-16 15:36:14 +0200

    Inline SpectrumStateBuilder class

--- src/gui/spectrum/state/SpectrumManager.java
+++ src/gui/spectrum/state/SpectrumManager.java
@@ -1,9 +1,7 @@
 package gui.spectrum.state;
 
 import frequency.Frequency;
-import gui.buckets.BucketHistory;
-import gui.buckets.Buckets;
-import gui.buckets.PrecalculatedBucketHistory;
+import gui.buckets.*;
 import gui.spectrum.SpectrumWindow;
 import harmonics.Harmonic;
 import harmonics.HarmonicCalculator;
@@ -15,9 +13,7 @@ import time.PerformanceTracker;
 import time.TimeInNanoSeconds;
 import time.TimeKeeper;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class SpectrumManager implements Runnable {
     private final SpectrumWindow spectrumWindow;
@@ -30,6 +26,8 @@ public class SpectrumManager implements Runnable {
     private final InputPort<VolumeState> volumeStateInput;
     private final OutputPort<Buckets> notesOutput;
     private final OutputPort<Buckets> harmonicsOutput;
+    private Map<Frequency, Double> newPairs;
+    private Set<Frequency> frequencies;
 
     public SpectrumManager(SpectrumWindow spectrumWindow, HarmonicCalculator harmonicCalculator, BoundedBuffer<TimeInNanoSeconds> frameEndTimeInputBuffer, BoundedBuffer<VolumeState> volumeStateBuffer, BoundedBuffer<Buckets> notesOutputBuffer, BoundedBuffer<Buckets> harmonicsOutputBuffer) {
         this.spectrumWindow = spectrumWindow;
@@ -66,16 +64,24 @@ public class SpectrumManager implements Runnable {
             Map<Frequency, Double> volumes = volumeState.volumes;
             Set<Frequency> liveFrequencies = volumes.keySet();
             Iterator<Map.Entry<Harmonic, Double>> harmonicHierarchyIterator = harmonicCalculator.getHarmonicHierarchyIterator(liveFrequencies, volumes);
-            SpectrumStateBuilder spectrumStateBuilder = new SpectrumStateBuilder(spectrumWindow, liveFrequencies, volumes, harmonicHierarchyIterator);
+            Buckets noteBuckets = toBuckets(liveFrequencies, volumes).precalculate();
+            newPairs = new HashMap<>();
+            frequencies = new HashSet<>();
             PerformanceTracker.stopTracking(timeKeeper);
 
             timeKeeper = PerformanceTracker.startTracking("build spectrum snapshot");
+            int counter = 0;
             while (TimeInNanoSeconds.now().lessThan(frameEndTime)) {
-                if (spectrumStateBuilder.update()) break;
+                if (update(harmonicHierarchyIterator)) break;
+                counter++;
+            }
+            System.out.println(counter);
+            if(!TimeInNanoSeconds.now().lessThan(frameEndTime)){
+                System.out.println("ran out of time");
             }
             PerformanceTracker.stopTracking(timeKeeper);
 
-            spectrumState = spectrumStateBuilder.finish();
+            spectrumState = finish(noteBuckets);
 
             timeKeeper = PerformanceTracker.startTracking("add to bucketHistory");
             bucketHistory = bucketHistory.addNewBuckets(spectrumState.harmonicsBuckets);
@@ -92,4 +98,53 @@ public class SpectrumManager implements Runnable {
             e.printStackTrace();
         }
     }
+
+    //return true when harmonicHierarchy has been depleted.
+    private boolean update(Iterator<Map.Entry<Harmonic, Double>> harmonicHierarchyIterator) {
+        try {
+            Map.Entry<Harmonic, Double> harmonicVolume = harmonicHierarchyIterator.next();
+            Frequency frequency = harmonicVolume.getKey().getFrequency();
+
+            if(spectrumWindow.inBounds(frequency)) {
+                frequencies.add(frequency);
+                Double newValue;
+                try {
+                    newValue = newPairs.get(frequency) + harmonicVolume.getValue();
+                } catch (NullPointerException e) {
+                    newValue = harmonicVolume.getValue();
+                }
+                newPairs.put(frequency, newValue);
+            }
+        } catch (NoSuchElementException e) {
+            return true;
+        }
+        return false;
+    }
+
+    private SpectrumState finish(Buckets noteBuckets) {
+        TimeKeeper timeKeeper = PerformanceTracker.startTracking("harmonics toBuckets");
+        Buckets newHarmonicsBuckets = toBuckets(frequencies, newPairs);
+        PerformanceTracker.stopTracking(timeKeeper);
+
+        timeKeeper = PerformanceTracker.startTracking("create SpectrumState");
+        SpectrumState spectrumState = new SpectrumState(noteBuckets, newHarmonicsBuckets);
+        PerformanceTracker.stopTracking(timeKeeper);
+
+        return spectrumState;
+    }
+
+    private Buckets toBuckets(Set<Frequency> keys, Map<Frequency, Double> map){
+        Set<Integer> indices = new HashSet<>();
+        Map<Integer, Bucket> entries = new HashMap<>();
+
+        for(Frequency frequency : keys){
+            int x = spectrumWindow.getX(frequency);
+
+            AtomicBucket bucket = new AtomicBucket(frequency, map.get(frequency));
+
+            Buckets.fill(indices, entries, x, bucket);
+        }
+        return new Buckets(indices, entries).precalculate();
+    }
+
 }
