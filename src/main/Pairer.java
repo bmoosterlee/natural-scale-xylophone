8b41b1034fdd859c0d95d226deb3aba420495866
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-07 01:48:20 +0100

    Add general purpose Pairer component to combine two buffer into one

---
+++ src/main/Pairer.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.AbstractMap.SimpleImmutableEntry;
+
+public class Pairer<K, V> extends Component {
+
+    private final InputPort<K> inputPort1;
+    private final InputPort<V> inputPort2;
+    private final OutputPort<SimpleImmutableEntry<K, V>> outputPort;
+
+    public Pairer(BoundedBuffer<K> inputBuffer1, BoundedBuffer<V> inputBuffer2, BoundedBuffer<SimpleImmutableEntry<K, V>> outputBuffer){
+        inputPort1 = new InputPort<>(inputBuffer1);
+        inputPort2 = new InputPort<>(inputBuffer2);
+        outputPort = new OutputPort<>(outputBuffer);
+
+        start();
+    }
+
+    @Override
+    protected void tick() {
+        try {
+            K consumed1 = inputPort1.consume();
+            V consumed2 = inputPort2.consume();
+            SimpleImmutableEntry<K, V> result = new SimpleImmutableEntry<>(consumed1, consumed2);
+            outputPort.produce(result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
