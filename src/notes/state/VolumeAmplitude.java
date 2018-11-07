d5604a5591c6200c8e7e6bd778899a3cfb05bab0
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-07 01:48:56 +0100

    Implement static sum method

--- src/notes/state/VolumeAmplitude.java
+++ src/notes/state/VolumeAmplitude.java
@@ -1,13 +1,27 @@
 package notes.state;
 
+import java.util.Collection;
 //todo move to composite volume. Do this by having a volume object which either has atomic volume (value) or composite volume (set)
 public class VolumeAmplitude {
     public final Double volume;
-    private final Double amplitude;
+    public final Double amplitude;
 
     VolumeAmplitude(Double volume, Double amplitude) {
         this.volume = volume;
         this.amplitude = amplitude;
+    }
+
+    static VolumeAmplitude sum(Collection<VolumeAmplitude> volumeAmplitudeCollection) {
+        VolumeAmplitude total = null;
+        for(VolumeAmplitude element : volumeAmplitudeCollection){
+            try {
+                total = total.add(element.volume);
+            }
+            catch(NullPointerException e){
+                total = element;
+            }
+        }
+        return total;
     }
 
     public VolumeAmplitude add(Double other) {
