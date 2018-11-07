d65bf48b014eef66424b6d4e2cc0ac28591c0a89
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-07 01:47:49 +0100

    Refactor PipeComponent static method to constructor

--- src/main/PipeComponent.java
+++ src/main/PipeComponent.java
@@ -1,27 +1,46 @@
 package main;
 
-public abstract class PipeComponent<K, V> extends Component {
+import java.util.AbstractMap;
+
+public class PipeComponent<K, V> extends Component {
 
     protected final InputPort<K> input;
     protected final OutputPort<V> output;
+    protected final CallableWithArguments<K, V> method;
 
-    public PipeComponent(BoundedBuffer<K> inputBuffer, BoundedBuffer<V> outputBuffer){
+    public PipeComponent(BoundedBuffer<K> inputBuffer, BoundedBuffer<V> outputBuffer, CallableWithArguments<K, V> method){
+        this.method = method;
+
         input = new InputPort<>(inputBuffer);
         output = new OutputPort<>(outputBuffer);
+
+        start();
     }
 
-    public static <K, V> PipeComponent<K, V> methodToComponent(CallableWithArguments<K, V> method, BoundedBuffer<K> inputBuffer, BoundedBuffer<V> outputBuffer){
-        return new PipeComponent<>(inputBuffer, outputBuffer) {
-            @Override
-            protected void tick() {
-                try {
-                    K consumed = input.consume();
-                    V result = method.call(consumed);
-                    output.produce(result);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+    @Override
+    protected void tick() {
+        try {
+            K consumed = input.consume();
+            V result = method.call(consumed);
+            output.produce(result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static <K, V> AbstractMap.SimpleImmutableEntry<BoundedBuffer<K>, BoundedBuffer<V>> methodToComponentBuffers(CallableWithArguments<K, V> method, int capacity, String name){
+        BoundedBuffer<K> inputBuffer = new BoundedBuffer<>(capacity, name + " input");
+        BoundedBuffer<V> outputBuffer = new BoundedBuffer<>(capacity, name + " output");
+        new PipeComponent<>(inputBuffer, outputBuffer, method);
+        return new AbstractMap.SimpleImmutableEntry<>(inputBuffer, outputBuffer);
+    }
+
+    public static <K, V> AbstractMap.SimpleImmutableEntry<OutputPort<K>, InputPort<V>> methodToComponentPorts(CallableWithArguments<K, V> method, int capacity, String name){
+        AbstractMap.SimpleImmutableEntry<BoundedBuffer<K>, BoundedBuffer<V>> buffers = PipeComponent.methodToComponentBuffers(method, capacity, name);
+        BoundedBuffer<K> inputBuffer = buffers.getKey();
+        BoundedBuffer<V> outputBuffer = buffers.getValue();
+        OutputPort<K> outputPort = new OutputPort<>(inputBuffer);
+        InputPort<V> inputPort = new InputPort<>(outputBuffer);
+        return new AbstractMap.SimpleImmutableEntry<>(outputPort, inputPort);
     }
 }
