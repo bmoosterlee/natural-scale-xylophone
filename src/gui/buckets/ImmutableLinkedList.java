c0e4f351324e6fc8901312559df1351974bd1a41
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-08 22:08:37 +0100

    Implement general purpose immutable list

---
+++ src/gui/buckets/ImmutableLinkedList.java
@@ -0,0 +1,33 @@
+package gui.buckets;
+
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.LinkedList;
+
+public class ImmutableLinkedList<T> {
+    private final LinkedList<T> list;
+
+    public ImmutableLinkedList(){
+        list = new LinkedList<>();
+    }
+
+    public ImmutableLinkedList(LinkedList<T> list){
+        this.list = list;
+    }
+
+    public ImmutableLinkedList<T> add(T element){
+        LinkedList<T> newList = new LinkedList<>(list);
+        newList.add(element);
+        return new ImmutableLinkedList<>(newList);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public SimpleImmutableEntry<ImmutableLinkedList<T>, T> poll() {
+        LinkedList<T> newList = new LinkedList<>(list);
+        T element = newList.poll();
+        ImmutableLinkedList<T> newImmutableLinkedList = new ImmutableLinkedList<>(newList);
+        return new SimpleImmutableEntry<>(newImmutableLinkedList, element);
+    }
+}
