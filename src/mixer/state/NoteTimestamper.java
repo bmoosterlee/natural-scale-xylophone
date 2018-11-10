cc53e31f89c6ce7dc294d4c3a3b7f865c3d49db5
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-10 23:41:17 +0100

    Refactor NoteTimeStamper to Component

--- src/mixer/state/NoteTimestamper.java
+++ src/mixer/state/NoteTimestamper.java
@@ -3,17 +3,49 @@ package mixer.state;
 import component.*;
 import frequency.Frequency;
 
-public class NoteTimestamper implements Component {
+public class NoteTimestamper extends TickablePipeComponent {
 
     public NoteTimestamper(BoundedBuffer<Long> sampleCountBuffer, BoundedBuffer<Frequency> newNoteBuffer, BoundedBuffer<TimestampedFrequencies> outputBuffer) {
-        BoundedBuffer<Long>[] broadcast = sampleCountBuffer.broadcast(2).toArray(new BoundedBuffer[0]);
-        broadcast[0]
+        super(sampleCountBuffer, outputBuffer, NoteTimestamper.build(newNoteBuffer));
+    }
+
+    public static CallableWithArguments<Long, TimestampedFrequencies> build(BoundedBuffer<Frequency> newNoteBuffer) {
+        return new CallableWithArguments<>() {
+            final OutputPort<Long> methodInputPort;
+            final InputPort<TimestampedFrequencies> methodOutputPort;
+
+            {
+                BoundedBuffer<Long> sampleCountBuffer = new BoundedBuffer<>(1, "noteTimestamper - input");
+                methodInputPort = sampleCountBuffer.createOutputPort();
+
+                BoundedBuffer<Long>[] broadcast = sampleCountBuffer.broadcast(2).toArray(new BoundedBuffer[0]);
+                methodOutputPort =
+                broadcast[0]
                 .pairWith(
-                        broadcast[1]
-                        .performMethod(input -> new Pulse())
-                        .performMethod(Flusher.flush(newNoteBuffer)))
-                .performMethod(input -> new TimestampedFrequencies(input.getKey(), input.getValue()))
-                .relayTo(outputBuffer);
+                    broadcast[1]
+                    .performMethod(input1 -> new Pulse())
+                    .performMethod(Flusher.flush(newNoteBuffer)))
+                .performMethod(
+                        input1 ->
+                        new TimestampedFrequencies(
+                            input1.getKey(),
+                            input1.getValue())).createInputPort();
+            }
+
+            @Override
+            public TimestampedFrequencies call(Long input) {
+                try {
+                    methodInputPort.produce(input);
+
+                    TimestampedFrequencies result = methodOutputPort.consume();
+
+                    return result;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
     }
 
 }
