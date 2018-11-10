d7ceffe028466d688ce3c9c8b693321eb20a893f
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-10 14:33:39 +0100

    Refactor to component

--- src/mixer/state/VolumeAmplitudeToVolumeFilter.java
+++ src/mixer/state/VolumeAmplitudeToVolumeFilter.java
@@ -1,50 +1,24 @@
 package mixer.state;
 
+import component.*;
 import frequency.Frequency;
-import component.BoundedBuffer;
-import component.InputPort;
-import component.OutputPort;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class VolumeAmplitudeToVolumeFilter implements Runnable{
-
-    private final InputPort<VolumeAmplitudeState> volumeAmplitudeStateInput;
-    private final OutputPort<VolumeState> volumeStateOutput;
+public class VolumeAmplitudeToVolumeFilter extends TickablePipeComponent<VolumeAmplitudeState, VolumeState> {
 
     public VolumeAmplitudeToVolumeFilter(BoundedBuffer<VolumeAmplitudeState> volumeAmplitudeStateBuffer, BoundedBuffer<VolumeState> volumeStateBuffer) {
-        volumeAmplitudeStateInput = new InputPort<>(volumeAmplitudeStateBuffer);
-        volumeStateOutput = new OutputPort<>(volumeStateBuffer);
-
-        start();
+        super(volumeAmplitudeStateBuffer, volumeStateBuffer, VolumeAmplitudeToVolumeFilter::filter);
     }
 
-    private void start() {
-        new Thread(this).start();
-    }
+    public static VolumeState filter(VolumeAmplitudeState volumeAmplitudeState) {
+        Map<Frequency, Double> volumeMap = new HashMap<>();
 
-    @Override
-    public void run() {
-        while(true){
-            tick();
+        for(Frequency frequency : volumeAmplitudeState.volumeAmplitudes.keySet()){
+            volumeMap.put(frequency, volumeAmplitudeState.volumeAmplitudes.get(frequency).volume);
         }
-    }
 
-    private void tick() {
-        try {
-            VolumeAmplitudeState volumeAmplitudeState = volumeAmplitudeStateInput.consume();
-
-            Map<Frequency, Double> volumeMap = new HashMap<>();
-
-            for(Frequency frequency : volumeAmplitudeState.volumeAmplitudes.keySet()){
-                volumeMap.put(frequency, volumeAmplitudeState.volumeAmplitudes.get(frequency).volume);
-            }
-
-            volumeStateOutput.produce(new VolumeState(volumeMap));
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        return new VolumeState(volumeMap);
     }
 }
