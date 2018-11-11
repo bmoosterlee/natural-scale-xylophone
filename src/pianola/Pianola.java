ddea6f4dca74b884fc2e78a99faf2e426624a45e
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-11 12:40:47 +0100

    Refactor to component, use buffer chaining

--- src/pianola/Pianola.java
+++ src/pianola/Pianola.java
@@ -5,101 +5,122 @@ package pianola;/*todo write a history tracker of when notes were played. Take a
  * todo let the pianola take the rhythmic harmonic value in real time and play that.
  * todo use a lookahead of the length of the shortest frame for the pianola, and play the maximum within that frame*/
 
-import component.Unpairer;
+import component.*;
 import frequency.Frequency;
-import spectrum.buckets.*;
-import component.BoundedBuffer;
-import component.InputPort;
-import component.OutputPort;
-import component.Pulse;
 import pianola.patterns.PianolaPattern;
+import spectrum.buckets.BucketHistory;
+import spectrum.buckets.Buckets;
+import spectrum.buckets.BucketsAverager;
+import spectrum.buckets.PrecalculatedBucketHistory;
 
 import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.Set;
 
-public class Pianola implements Runnable{
-    private final PianolaPattern pianolaPattern;
-
-    private final InputPort<Buckets> notesInput;
-    private BucketHistory noteHistory;
-    private final int repetitionDampener;
-    private final OutputPort<Buckets> averagerInput;
-
-    private final InputPort<Pulse> pulseInput;
-    private final InputPort<Buckets> preparedNotesInput;
-    private final InputPort<Buckets> preparedHarmonicsInput;
-    private final OutputPort<Frequency> playedNotes;
+public class Pianola extends TickablePipeComponent<Pulse, Frequency>{
 
     public Pianola(BoundedBuffer<Pulse> tickBuffer, BoundedBuffer<AbstractMap.SimpleImmutableEntry<Buckets, Buckets>> spectrumBuffer, BoundedBuffer<Frequency> outputBuffer, PianolaPattern pianolaPattern, int inaudibleFrequencyMargin) {
-        this.pianolaPattern = pianolaPattern;
-        
-        int count = 0;
-        int capacity = 1000;
-
-        repetitionDampener = 3;
-        noteHistory = new PrecalculatedBucketHistory(50);
-
-        BoundedBuffer<Buckets> noteSpectrumBuffer = new BoundedBuffer<>(capacity, "pianola - note spectrum");
-        BoundedBuffer<Buckets> harmonicSpectrumBuffer = new BoundedBuffer<>(capacity, "pianola - harmonic spectrum");
-        new Unpairer<>(spectrumBuffer, noteSpectrumBuffer, harmonicSpectrumBuffer);
-
-        BoundedBuffer<Buckets> notesAveragerInputBuffer = new BoundedBuffer<>(capacity, "Pianola" + String.valueOf(count)); count++;
-        BoundedBuffer<Buckets> notesAveragerOutputBuffer = new BoundedBuffer<>(capacity, "Pianola" + String.valueOf(count)); count++;
-        new BucketsAverager(2* inaudibleFrequencyMargin, notesAveragerInputBuffer, notesAveragerOutputBuffer);
-        averagerInput = new OutputPort<>(notesAveragerInputBuffer);
-
-        BoundedBuffer<Buckets> harmonicsAveragerBuffer = new BoundedBuffer<>(capacity, "Pianola" + String.valueOf(count)); count++;
-        new BucketsAverager(inaudibleFrequencyMargin, harmonicSpectrumBuffer, harmonicsAveragerBuffer);
-
-        pulseInput = new InputPort<>(tickBuffer);
-        notesInput = new InputPort<>(noteSpectrumBuffer);
-        preparedNotesInput = new InputPort<>(notesAveragerOutputBuffer);
-        preparedHarmonicsInput = new InputPort<>(harmonicsAveragerBuffer);
-        playedNotes = new OutputPort<>(outputBuffer);
-
-        start();
+        super(tickBuffer, outputBuffer, build(spectrumBuffer, pianolaPattern,inaudibleFrequencyMargin));
     }
 
-    private void start() {
-        new Thread(this).start();
-    }
+    public static CallableWithArguments<Pulse, Frequency> build(BoundedBuffer<AbstractMap.SimpleImmutableEntry<Buckets, Buckets>> spectrumBuffer, PianolaPattern pianolaPattern, int inaudibleFrequencyMargin){
+        return new CallableWithArguments<>() {
+            private final InputPort<Buckets> notesInput;
+            private BucketHistory noteHistory;
+            private final int repetitionDampener;
+            private final OutputPort<Buckets> averagerInput;
 
+            private final InputPort<Buckets> preparedNotesInput;
+            private final InputPort<Buckets> preparedHarmonicsInput;
 
-    @Override
-    public void run() {
-        while(true){
-            tick();
-        }
-    }
+            private InputPort<Frequency> methodOutputPort;
 
-    private void tick() {
-        try {
-            pulseInput.consume();
+            private OutputPort<Collection<Frequency>> outputPort;
 
-            Buckets origNoteBuckets;
-            try {
-                origNoteBuckets = notesInput.consume();
+            {
+                int count = 0;
+                int capacity = 1000;
+
+                repetitionDampener = 3;
+                noteHistory = new PrecalculatedBucketHistory(50);
+
+                BoundedBuffer<Buckets> noteSpectrumBuffer = new BoundedBuffer<>(capacity, "pianola - note spectrum");
+                BoundedBuffer<Buckets> harmonicSpectrumBuffer = new BoundedBuffer<>(capacity, "pianola - harmonic spectrum");
+                new Unpairer<>(spectrumBuffer, noteSpectrumBuffer, harmonicSpectrumBuffer);
+
+                BoundedBuffer<Buckets> notesAveragerInputBuffer = new BoundedBuffer<>(capacity, "Pianola" + String.valueOf(count));
+                count++;
+                BoundedBuffer<Buckets> notesAveragerOutputBuffer = new BoundedBuffer<>(capacity, "Pianola" + String.valueOf(count));
+                count++;
+                new BucketsAverager(2 * inaudibleFrequencyMargin, notesAveragerInputBuffer, notesAveragerOutputBuffer);
+                averagerInput = new OutputPort<>(notesAveragerInputBuffer);
+
+                BoundedBuffer<Buckets> harmonicsAveragerBuffer = new BoundedBuffer<>(capacity, "Pianola" + String.valueOf(count));
+                count++;
+                new BucketsAverager(inaudibleFrequencyMargin, harmonicSpectrumBuffer, harmonicsAveragerBuffer);
+
+                BoundedBuffer<Collection<Frequency>> patternOutput = new BoundedBuffer<>(1, "pianola - output");
+                outputPort = patternOutput.createOutputPort();
+                BoundedBuffer<Frequency> methodOutput = toBuffer(patternOutput);
+                methodOutputPort = methodOutput.createInputPort();
+
+                notesInput = new InputPort<>(noteSpectrumBuffer);
+                preparedNotesInput = new InputPort<>(notesAveragerOutputBuffer);
+                preparedHarmonicsInput = new InputPort<>(harmonicsAveragerBuffer);
             }
-            catch(NullPointerException e){
-                origNoteBuckets = new Buckets();
-            }
-            noteHistory = noteHistory.addNewBuckets(origNoteBuckets);
 
-            Buckets notesBeforeAveraging = noteHistory.getTimeAveragedBuckets().multiply(repetitionDampener);
-            averagerInput.produce(notesBeforeAveraging);
-
-            Buckets noteBuckets = preparedNotesInput.consume();
-            Buckets harmonicsBuckets = preparedHarmonicsInput.consume();
-
-            for(Frequency frequency : pianolaPattern.playPattern(noteBuckets, harmonicsBuckets)){
+            private Frequency playNotes(Pulse input) {
                 try {
-                    playedNotes.produce(frequency);
+                    Buckets origNoteBuckets;
+                    try {
+                        origNoteBuckets = notesInput.consume();
+                    } catch (NullPointerException e) {
+                        origNoteBuckets = new Buckets();
+                    }
+                    noteHistory = noteHistory.addNewBuckets(origNoteBuckets);
+
+                    Buckets notesBeforeAveraging = noteHistory.getTimeAveragedBuckets().multiply(repetitionDampener);
+                    averagerInput.produce(notesBeforeAveraging);
+
+                    Buckets noteBuckets = preparedNotesInput.consume();
+                    Buckets harmonicsBuckets = preparedHarmonicsInput.consume();
+
+                    Set<Frequency> results = pianolaPattern.playPattern(noteBuckets, harmonicsBuckets);
+                    outputPort.produce(results);
+
+                    return methodOutputPort.consume();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                catch(NullPointerException ignored) {
-                }
+                return null;
             }
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            @Override
+            public Frequency call(Pulse input) {
+                return playNotes(input);
+            }
+        };
+    }
+
+    public static <T> BoundedBuffer<T> toBuffer(BoundedBuffer<Collection<T>> input){
+        BoundedBuffer<T> outputBuffer = new BoundedBuffer<>(1, "toBuffer - output");
+        InputPort<Collection<T>> inputPort = input.createInputPort();
+        OutputPort<T> outputPort = outputBuffer.createOutputPort();
+
+        new Tickable() {
+            @Override
+            protected void tick() {
+                try {
+                    Collection<T> input = inputPort.consume();
+                    for(T element : input){
+                        outputPort.produce(element);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        return outputBuffer;
     }
 }
