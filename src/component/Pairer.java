0f9432b61903e847ebdb34089bc5ab205180be04
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-11 21:17:53 +0100

    Refactor to component

--- src/component/Pairer.java
+++ src/component/Pairer.java
@@ -2,31 +2,20 @@ package component;
 
 import java.util.AbstractMap.SimpleImmutableEntry;
 
-public class Pairer<K, V> extends Tickable {
-
-    private final InputPort<K> inputPort1;
-    private final InputPort<V> inputPort2;
-    private final OutputPort<SimpleImmutableEntry<K, V>> outputPort;
+public class Pairer<K, V> extends TickablePipeComponent<K, SimpleImmutableEntry<K, V>> {
 
     public Pairer(BoundedBuffer<K> inputBuffer1, BoundedBuffer<V> inputBuffer2, BoundedBuffer<SimpleImmutableEntry<K, V>> outputBuffer){
-        inputPort1 = new InputPort<>(inputBuffer1);
-        inputPort2 = new InputPort<>(inputBuffer2);
-        outputPort = new OutputPort<>(outputBuffer);
+        super(inputBuffer1, outputBuffer, build(inputBuffer2));
 
-        start();
     }
 
-    @Override
-    protected void tick() {
-        try {
-            K consumed1 = inputPort1.consume();
-            V consumed2 = inputPort2.consume();
-            SimpleImmutableEntry<K, V> result = new SimpleImmutableEntry<>(consumed1, consumed2);
-            outputPort.produce(result);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+    public static <K, V> CallableWithArguments<K, SimpleImmutableEntry<K, V>> build(BoundedBuffer<V> inputBuffer2){
+        return new CallableWithArguments<>() {
+            private final InputPort<V> inputPort2;
+
+            {
+                inputPort2 = new InputPort<>(inputBuffer2);
+            }
 
     public static <K, V> BoundedBuffer<SimpleImmutableEntry<K, V>> PairerWithOutputBuffer(BoundedBuffer<K> inputBuffer1, BoundedBuffer<V> inputBuffer2, int capacity, String name){
         BoundedBuffer<SimpleImmutableEntry<K,V>> outputBuffer = new BoundedBuffer<>(capacity, name);
