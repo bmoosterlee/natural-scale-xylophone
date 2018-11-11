6e29482d9985e61f1090df156d87e5b7ae9f9d5f
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-11 02:45:38 +0100

    Refactor Mixer to component, using buffer chaining

--- src/sound/SoundEnvironment.java
+++ src/sound/SoundEnvironment.java
@@ -1,7 +1,6 @@
 package sound;
 
-import component.BoundedBuffer;
-import component.InputPort;
+import component.*;
 import mixer.state.VolumeAmplitudeState;
 import time.PerformanceTracker;
 import time.TimeKeeper;
@@ -12,111 +11,107 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import java.util.List;
 
-public class SoundEnvironment implements Runnable{
-    private final SourceDataLine sourceDataLine;
-    private final int sampleSize;
-    private final double marginalSampleSize;
-
-    private final InputPort<Double> sampleAmplitudeInput;
+public class SoundEnvironment extends TickableInputComponent<VolumeAmplitudeState> {
 
     public SoundEnvironment(BoundedBuffer<VolumeAmplitudeState> inputBuffer, int SAMPLE_SIZE_IN_BITS, SampleRate sampleRate) {
-        sampleSize = (int) (Math.pow(2, SAMPLE_SIZE_IN_BITS) - 1);
-        marginalSampleSize = 1. / Math.pow(2, SAMPLE_SIZE_IN_BITS);
-
-
-        AudioFormat af = new AudioFormat((float) sampleRate.sampleRate, SAMPLE_SIZE_IN_BITS, 1, true, false);
-        SourceDataLine newSourceDataLine;
-        try {
-            newSourceDataLine = AudioSystem.getSourceDataLine(af);
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-            newSourceDataLine = null;
-        }
-        sourceDataLine = newSourceDataLine;
-        try {
-            sourceDataLine.open();
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        }
-        sourceDataLine.start();
-
-        BoundedBuffer<Double> amplitudeBuffer = new BoundedBuffer<>(1, "sound environment - signal");
-        new VolumeAmplitudeStateToSignal(inputBuffer, amplitudeBuffer);
-        sampleAmplitudeInput = new InputPort<>(amplitudeBuffer);
-
-        start();
+        super(inputBuffer, build(SAMPLE_SIZE_IN_BITS, sampleRate));
     }
 
-    private void start() {
-        new Thread(this).start();
-    }
+    public static CallableWithArgument<VolumeAmplitudeState> build(int SAMPLE_SIZE_IN_BITS, SampleRate sampleRate){
+        return new CallableWithArgument<>() {
+            private final SourceDataLine sourceDataLine;
+            private final int sampleSize;
+            private final double marginalSampleSize;
 
-    @Override
-    public void run() {
-        while(true){
-            tick();
-        }
-    }
+            private final InputPort<Double> sampleAmplitudeInput;
+            private final OutputPort<VolumeAmplitudeState> methodInput;
 
-    private void tick() {
-        try {
-            Double amplitude = sampleAmplitudeInput.consume();
+            {
+                sampleSize = (int) (Math.pow(2, SAMPLE_SIZE_IN_BITS) - 1);
+                marginalSampleSize = 1. / Math.pow(2, SAMPLE_SIZE_IN_BITS);
 
-            TimeKeeper totalTimeKeeper = PerformanceTracker.startTracking("sound environment play amplitude");
-            TimeKeeper timeKeeper = PerformanceTracker.startTracking("fit amplitude");
-            byte fittedAmplitude = fitAmplitude(amplitude);
-            PerformanceTracker.stopTracking(timeKeeper);
+                AudioFormat af = new AudioFormat((float) sampleRate.sampleRate, SAMPLE_SIZE_IN_BITS, 1, true, false);
+                SourceDataLine newSourceDataLine;
+                try {
+                    newSourceDataLine = AudioSystem.getSourceDataLine(af);
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                    newSourceDataLine = null;
+                }
+                sourceDataLine = newSourceDataLine;
+                try {
+                    sourceDataLine.open();
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+                sourceDataLine.start();
 
-            timeKeeper = PerformanceTracker.startTracking("write to buffer");
-            writeToBuffer(fittedAmplitude);
-            PerformanceTracker.stopTracking(timeKeeper);
-            PerformanceTracker.stopTracking(totalTimeKeeper);
+                BoundedBuffer<VolumeAmplitudeState> methodInputBuffer = new BoundedBuffer<>(1, "sound environment - input");
+                methodInput = methodInputBuffer.createOutputPort();
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void writeToBuffer(byte amplitude) {
-        sourceDataLine.write(new byte[]{amplitude}, 0, 1);
-    }
-
-    private void tickBatch() {
-        try {
-            List<Double> amplitudes = sampleAmplitudeInput.flushOrConsume();
-            int size = amplitudes.size();
-
-            TimeKeeper timeKeeper = PerformanceTracker.startTracking("fit amplitude");
-            int i = 0;
-            byte[] fittedAmplitudes = new byte[size];
-            for(Double amplitude : amplitudes) {
-                fittedAmplitudes[i++] = fitAmplitude(amplitude);
+                BoundedBuffer<Double> amplitudeBuffer = new BoundedBuffer<>(1, "sound environment - signal");
+                new VolumeAmplitudeStateToSignal(methodInputBuffer, amplitudeBuffer);
+                sampleAmplitudeInput = new InputPort<>(amplitudeBuffer);
             }
-            PerformanceTracker.stopTracking(timeKeeper);
 
-            timeKeeper = PerformanceTracker.startTracking("write to buffer");
-            writeToBuffer(fittedAmplitudes, size);
-            PerformanceTracker.stopTracking(timeKeeper);
+            private void playSound(VolumeAmplitudeState input) {
+                try {
+                    methodInput.produce(input);
+                    Double amplitude = sampleAmplitudeInput.consume();
+                    byte fittedAmplitude = fitAmplitude(amplitude);
+                    writeToBuffer(fittedAmplitude);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+            private void writeToBuffer(byte amplitude) {
+                sourceDataLine.write(new byte[]{amplitude}, 0, 1);
+            }
 
-    private void writeToBuffer(byte[] amplitudes, int size) {
-        sourceDataLine.write(amplitudes, 0, size);
-    }
+            private void tickBatch() {
+                try {
+                    List<Double> amplitudes = sampleAmplitudeInput.flushOrConsume();
+                    int size = amplitudes.size();
 
-    private byte fitAmplitude(double amplitude) {
-        return (byte) Math.max(Byte.MIN_VALUE, Math.min(Byte.MAX_VALUE, (byte) Math.floor(sampleSize * amplitude / 2)));
-    }
+                    TimeKeeper timeKeeper = PerformanceTracker.startTracking("fit amplitude");
+                    int i = 0;
+                    byte[] fittedAmplitudes = new byte[size];
+                    for (Double amplitude : amplitudes) {
+                        fittedAmplitudes[i++] = fitAmplitude(amplitude);
+                    }
+                    PerformanceTracker.stopTracking(timeKeeper);
 
-    public boolean isAudible(Double volume) {
-        return volume >= marginalSampleSize;
-    }
+                    timeKeeper = PerformanceTracker.startTracking("write to buffer");
+                    writeToBuffer(fittedAmplitudes, size);
+                    PerformanceTracker.stopTracking(timeKeeper);
 
-    public void close() {
-        sourceDataLine.drain();
-        sourceDataLine.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            private void writeToBuffer(byte[] amplitudes, int size) {
+                sourceDataLine.write(amplitudes, 0, size);
+            }
+
+            private byte fitAmplitude(double amplitude) {
+                return (byte) Math.max(Byte.MIN_VALUE, Math.min(Byte.MAX_VALUE, (byte) Math.floor(sampleSize * amplitude / 2)));
+            }
+
+//            public boolean isAudible(Double volume) {
+//                return volume >= marginalSampleSize;
+//            }
+//
+//            public void close() {
+//                sourceDataLine.drain();
+//                sourceDataLine.stop();
+//            }
+
+            @Override
+            public void call(VolumeAmplitudeState input) {
+                playSound(input);
+            }
+        };
     }
 }
