67e3798fe64897c1cc6a37194c7417ccf4fb6999
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-11 21:12:24 +0100

    Refactor Unpairer to component

--- src/component/Unpairer.java
+++ src/component/Unpairer.java
@@ -1,37 +1,42 @@
 package component;
 
+import java.util.AbstractMap;
 import java.util.AbstractMap.SimpleImmutableEntry;
 
-public class Unpairer<K, V> extends Tickable {
-
-    private final InputPort<SimpleImmutableEntry<K, V>> inputPort;
-    private final OutputPort<K> outputPort1;
-    private final OutputPort<V> outputPort2;
+public class Unpairer<K, V> extends TickablePipeComponent<SimpleImmutableEntry<K, V>, V> {
 
     public Unpairer(BoundedBuffer<SimpleImmutableEntry<K, V>> inputBuffer, BoundedBuffer<K> outputBuffer1, BoundedBuffer<V> outputBuffer2){
-        inputPort = new InputPort<>(inputBuffer);
-        outputPort1 = new OutputPort<>(outputBuffer1);
-        outputPort2 = new OutputPort<>(outputBuffer2);
-
-        start();
+        super(inputBuffer, outputBuffer2, build(outputBuffer1));
     }
 
-    @Override
-    protected void tick() {
-        try {
-            SimpleImmutableEntry<K, V> consumed = inputPort.consume();
-            K result1 = consumed.getKey();
-            V result2 = consumed.getValue();
-            outputPort1.produce(result1);
-            outputPort2.produce(result2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+    public static <K, V> CallableWithArguments<SimpleImmutableEntry<K, V>, V> build(BoundedBuffer<K> outputBuffer1){
+        return new CallableWithArguments<>() {
+            private final OutputPort<K> outputPort1;
+
+            {
+                outputPort1 = new OutputPort<>(outputBuffer1);
+            }
+
+            private V unpair(SimpleImmutableEntry<K, V> consumed) {
+                try {
+                    K result1 = consumed.getKey();
+                    V result2 = consumed.getValue();
+                    outputPort1.produce(result1);
+                    return result2;
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            public V call(SimpleImmutableEntry<K, V> input) {
+                return unpair(input);
+            }
+        };
     }
 
-    public static <K, V> BoundedBuffer<SimpleImmutableEntry<K, V>> UnpairerWithInputBuffer(BoundedBuffer<K> outputBuffer1, BoundedBuffer<V> outputBuffer2, int capacity, String name){
-        BoundedBuffer<SimpleImmutableEntry<K,V>> inputBuffer = new BoundedBuffer<>(capacity, name);
-        new Unpairer<>(inputBuffer, outputBuffer1, outputBuffer2);
-        return inputBuffer;
-    }
+
 }
