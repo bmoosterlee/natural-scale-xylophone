9ed80efe718efb06894231213c824b222f8b4026
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-12 18:38:17 +0100

    Refactor to component

--- src/spectrum/buckets/Transposer.java
+++ src/spectrum/buckets/Transposer.java
@@ -1,36 +1,14 @@
 package spectrum.buckets;
 
-import component.buffer.BoundedBuffer;
-import component.buffer.InputPort;
-import component.buffer.OutputPort;
-import component.buffer.SimpleBuffer;
+import component.buffer.*;
+import component.utilities.RunningPipeComponent;
 import component.utilities.TickRunner;
 
-public class Transposer extends TickRunner {
-
-    private final int transposition;
-
-    private final InputPort<Buckets> input;
-    private final OutputPort<Buckets> output;
+public class Transposer extends RunningPipeComponent<Buckets, Buckets> {
 
     public Transposer(BoundedBuffer<Buckets> inputBuffer, SimpleBuffer<Buckets> outputBuffer, int transposition) {
-        this.transposition = transposition;
-
-        input = new InputPort<>(inputBuffer);
-        output = new OutputPort<>(outputBuffer);
+        super(inputBuffer, outputBuffer, input -> input.transpose(transposition));
 
         start();
-    }
-
-    protected void tick() {
-        try {
-            Buckets buckets = input.consume();
-            Buckets transposedBuckets = buckets.transpose(transposition);
-            output.produce(transposedBuckets);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
     }
 }
