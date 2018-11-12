d429416aee690c40e4bc28aab8520fc03256271b
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-12 18:37:54 +0100

    Refactor to component

--- src/spectrum/buckets/BucketHistoryComponent.java
+++ src/spectrum/buckets/BucketHistoryComponent.java
@@ -1,37 +1,22 @@
 package spectrum.buckets;
 
-import component.buffer.BoundedBuffer;
-import component.buffer.InputPort;
-import component.buffer.OutputPort;
-import component.buffer.SimpleBuffer;
+import component.buffer.*;
+import component.utilities.RunningPipeComponent;
 import component.utilities.TickRunner;
 
-public class BucketHistoryComponent extends TickRunner {
-    private BucketHistory bucketHistory;
-
-    private final InputPort<Buckets> inputPort;
-    private final OutputPort<Buckets> outputPort;
+public class BucketHistoryComponent extends RunningPipeComponent<Buckets, Buckets> {
 
     public BucketHistoryComponent(BoundedBuffer<Buckets> inputBuffer, SimpleBuffer<Buckets> outputBuffer, int size){
-        bucketHistory = new PrecalculatedBucketHistory(size);
+        super(inputBuffer, outputBuffer, new CallableWithArguments<>() {
+            BucketHistory bucketHistory = new PrecalculatedBucketHistory(size);
 
-        inputPort = new InputPort<>(inputBuffer);
-        outputPort = new OutputPort<>(outputBuffer);
-
-        start();
+            @Override
+            public Buckets call(Buckets input) {
+                bucketHistory = bucketHistory.addNewBuckets(input);
+                Buckets timeAveragedBuckets = bucketHistory.getTimeAveragedBuckets();
+                return timeAveragedBuckets;
+            }
+        });
     }
 
-    protected void tick() {
-        try {
-            Buckets newBuckets = inputPort.consume();
-
-            bucketHistory = bucketHistory.addNewBuckets(newBuckets);
-            Buckets timeAveragedBuckets = bucketHistory.getTimeAveragedBuckets();
-
-            outputPort.produce(timeAveragedBuckets);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
