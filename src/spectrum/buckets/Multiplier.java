ab78432ad45a7603acff46870d12f1cbcc846396
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-12 18:38:10 +0100

    Refactor to component

--- src/spectrum/buckets/Multiplier.java
+++ src/spectrum/buckets/Multiplier.java
@@ -1,38 +1,12 @@
 package spectrum.buckets;
 
-import component.buffer.BoundedBuffer;
-import component.buffer.InputPort;
-import component.buffer.OutputPort;
-import component.buffer.SimpleBuffer;
+import component.buffer.*;
+import component.utilities.RunningPipeComponent;
 import component.utilities.TickRunner;
 
-public class Multiplier extends TickRunner {
-
-    private final double multiplier;
-
-    private final InputPort<Buckets> input;
-    private final OutputPort<Buckets> output;
+public class Multiplier extends RunningPipeComponent<Buckets, Buckets> {
 
     public Multiplier(BoundedBuffer<Buckets> inputBuffer, SimpleBuffer<Buckets> outputBuffer, double multiplier) {
-        this.multiplier = multiplier;
-
-        input = new InputPort<>(inputBuffer);
-        output = new OutputPort<>(outputBuffer);
-
-        start();
-    }
-
-    protected void tick() {
-        try {
-            Buckets buckets = input.consume();
-
-            Buckets multipliedBuckets = buckets.multiply(multiplier);
-
-            output.produce(multipliedBuckets);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        super(inputBuffer, outputBuffer, input -> input.multiply(multiplier));
     }
 }
