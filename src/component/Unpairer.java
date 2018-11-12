8cabb0da090fb37585032734a341e8a50f7388bf
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-12 17:44:26 +0100

    Refactor Unpairer to not extend PipeComponent to prevent faulty buffer chaining

--- src/component/Unpairer.java
+++ src/component/Unpairer.java
@@ -1,47 +1,44 @@
 package component;
 
-import component.buffer.BoundedBuffer;
-import component.buffer.CallableWithArguments;
-import component.buffer.OutputPort;
-import component.buffer.SimpleBuffer;
-import component.utilities.RunningPipeComponent;
+import component.buffer.*;
+import component.utilities.TickRunner;
 
 import java.util.AbstractMap.SimpleImmutableEntry;
 
-public class Unpairer<K, V> extends RunningPipeComponent<SimpleImmutableEntry<K, V>, V> {
+public class Unpairer<K, V> {
+
+    protected final InputPort<SimpleImmutableEntry<K, V>> input;
+    private final OutputPort<K> output1;
+    private final OutputPort<V> output2;
+
+    TickRunner tickRunner = new MyTickRunner();
 
     public Unpairer(BoundedBuffer<SimpleImmutableEntry<K,V>> inputBuffer, SimpleBuffer<K> outputBuffer1, SimpleBuffer<V> outputBuffer2){
-        super(inputBuffer, outputBuffer2, build(outputBuffer1));
+        input = inputBuffer.createInputPort();
+        output1 = outputBuffer1.createOutputPort();
+        output2 = outputBuffer2.createOutputPort();
+
+        tickRunner.start();
     }
 
-
-    private static <K, V> CallableWithArguments<SimpleImmutableEntry<K, V>, V> build(SimpleBuffer<K> outputBuffer1){
-        return new CallableWithArguments<>() {
-            private final OutputPort<K> outputPort1;
-
-            {
-                outputPort1 = new OutputPort<>(outputBuffer1);
-            }
-
-            private V unpair(SimpleImmutableEntry<K, V> consumed) {
-                try {
-                    K result1 = consumed.getKey();
-                    V result2 = consumed.getValue();
-                    outputPort1.produce(result1);
-                    return result2;
-                }
-                catch(InterruptedException e){
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-            @Override
-            public V call(SimpleImmutableEntry<K, V> input) {
-                return unpair(input);
-            }
-        };
+    private class MyTickRunner extends TickRunner {
+        @Override
+        protected void tick() {
+            Unpairer.this.tick();
+        }
     }
 
+    protected void tick(){
+        try {
+            SimpleImmutableEntry<K, V> consumed = input.consume();
+            K result1 = consumed.getKey();
+            V result2 = consumed.getValue();
+            output1.produce(result1);
+            output2.produce(result2);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
 
 }
