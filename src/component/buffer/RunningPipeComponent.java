ff485f1d3a0ce59440f1c3463a0525179b8940ef
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-18 17:55:15 +0100

    Remove obsolete methods

--- src/component/buffer/RunningPipeComponent.java
+++ src/component/buffer/RunningPipeComponent.java
@@ -1,7 +1,5 @@
 package component.buffer;
 
-import java.util.AbstractMap;
-
 public class RunningPipeComponent<K, V> extends PipeComponent<K, V> {
 
     private final MyTickRunner tickRunner = new MyTickRunner();
@@ -25,19 +23,4 @@ public class RunningPipeComponent<K, V> extends PipeComponent<K, V> {
         tickRunner.start();
     }
 
-    public static <K, V> AbstractMap.SimpleImmutableEntry<BoundedBuffer<K>, BoundedBuffer<V>> methodToComponentBuffers(CallableWithArguments<K, V> method, int capacity, String name){
-        SimpleBuffer<K> inputBuffer = new SimpleBuffer<>(capacity, name + " input");
-        SimpleBuffer<V> outputBuffer = new SimpleBuffer<>(capacity, name + " output");
-        new RunningPipeComponent<>(inputBuffer, outputBuffer, method);
-        return new AbstractMap.SimpleImmutableEntry<>(inputBuffer, outputBuffer);
-    }
-
-    public static <K, V> AbstractMap.SimpleImmutableEntry<OutputPort<K>, InputPort<V>> methodToComponentPorts(CallableWithArguments<K, V> method, int capacity, String name){
-        AbstractMap.SimpleImmutableEntry<BoundedBuffer<K>, BoundedBuffer<V>> buffers = RunningPipeComponent.methodToComponentBuffers(method, capacity, name);
-        BoundedBuffer<K> inputBuffer = buffers.getKey();
-        BoundedBuffer<V> outputBuffer = buffers.getValue();
-        OutputPort<K> outputPort = new OutputPort<>(inputBuffer);
-        InputPort<V> inputPort = outputBuffer.createInputPort();
-        return new AbstractMap.SimpleImmutableEntry<>(outputPort, inputPort);
-    }
 }
