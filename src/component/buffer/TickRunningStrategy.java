3d5f915ff44cf6d3721f72ea5f189a5551fbec65
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-24 20:57:25 +0100

    Fix unchecked assignment

--- src/component/buffer/TickRunningStrategy.java
+++ src/component/buffer/TickRunningStrategy.java
@@ -1,16 +1,14 @@
 package component.buffer;
 
-import java.util.concurrent.Callable;
-
 public class TickRunningStrategy {
 
     public TickRunningStrategy(final AbstractComponent pipeComponent){
         this(pipeComponent, false);
     }
 
-    public TickRunningStrategy(final AbstractComponent pipeComponent, boolean threadAlreadyRunning){
+    public <K, V> TickRunningStrategy(final AbstractComponent<K, V> pipeComponent, boolean threadAlreadyRunning){
         if(pipeComponent.isParallelisable()){
-            new TickRunnerSpawner(pipeComponent, threadAlreadyRunning).start();
+            new TickRunnerSpawner<>(pipeComponent, threadAlreadyRunning).start();
         }
         else {
             new SimpleTickRunner(pipeComponent).start();
