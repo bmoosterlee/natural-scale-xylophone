7774f57717ba39d61de5093cfcc42b1bbd23c57f
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-30 00:02:17 +0100

    Refactor OutputComponentChainLink to no longer need a MethodOutputComponent construction parameter

--- src/component/buffer/OutputComponentChainLink.java
+++ src/component/buffer/OutputComponentChainLink.java
@@ -2,94 +2,104 @@ package component.buffer;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.Callable;
 
 public class OutputComponentChainLink<V> extends ComponentChainLink<Void, V> {
-    private final MethodOutputComponent<V> methodOutputComponent;
+    private final OutputCallable<V> method;
+    private final BoundedBuffer<V> outputBuffer;
 
-    private OutputComponentChainLink(MethodOutputComponent<V> methodOutputComponent) {
+    private OutputComponentChainLink(OutputCallable<V> method, BoundedBuffer<V> outputBuffer) {
         super(null);
-        this.methodOutputComponent = methodOutputComponent;
+        this.method = method;
+        this.outputBuffer = outputBuffer;
     }
 
     @Override
     protected void wrap() {
-        new TickRunningStrategy(methodOutputComponent);
+        new TickRunningStrategy(new MethodOutputComponent<>(outputBuffer, method));
     }
 
     @Override
     protected void wrap(PipeCallable<V, ?> nextMethodChain, BoundedBuffer outputBuffer, int chainLinks) {
         if(!isParallelisable()) {
-            new TickRunningStrategy(
-                    new MethodOutputComponent<>(outputBuffer, () -> {
-                        synchronized (this) {
-                            return nextMethodChain.call(methodOutputComponent.method.call());
-                        }
-                    }) {
-                        @Override
-                        public Collection<BoundedBuffer> getInputBuffers() {
-                            return Collections.singleton(getOutputPort().getBuffer());
-                        }
-                    }, chainLinks + 1);
+            OutputCallable<?> sequentialCallChain = () -> {
+                synchronized (this) {
+                    return nextMethodChain.call(method.call());
+                }
+            };
+            startChainedSequentialComponent(sequentialCallChain, outputBuffer, chainLinks);
+        } else {
+            OutputCallable<?> parallelCallChain = () -> nextMethodChain.call(method.call());
+            startChainedParallelComponent(parallelCallChain, outputBuffer);
         }
-        else{
-            new TickRunningStrategy(
-                    new MethodOutputComponent<>(outputBuffer, () -> nextMethodChain.call(methodOutputComponent.method.call())) {
-                        @Override
-                        public Collection<BoundedBuffer> getInputBuffers() {
-                            return Collections.singleton(getOutputPort().getBuffer());
-                        }
-                    });
-        }
+    }
+
+    private void startChainedSequentialComponent(OutputCallable<?> sequentialCallChain, BoundedBuffer outputBuffer, int chainLinks) {
+        new TickRunningStrategy(new MethodOutputComponent<>(outputBuffer, sequentialCallChain), chainLinks + 1);
+    }
+
+    private void startChainedParallelComponent(OutputCallable<?> parallelCallChain, BoundedBuffer outputBuffer) {
+        new TickRunningStrategy(new MethodOutputComponent<>(outputBuffer, parallelCallChain));
     }
 
     @Override
     protected void wrap(InputCallable<V> nextMethodChain, BoundedBuffer outputBuffer, int chainLinks) {
         if(!isParallelisable()) {
-            new TickRunningStrategy(
-                    new AbstractComponent<>() {
-                        @Override
-                        public Collection<BoundedBuffer<Object>> getInputBuffers() {
-                            return Collections.singleton(getOutputPort().getBuffer());
-                        }
-
-                        @Override
-                        public Collection<BoundedBuffer<Object>> getOutputBuffers() {
-                            return Collections.emptyList();
-                        }
-
-                        @Override
-                        protected void tick() {
-                            synchronized (this) {
-                                nextMethodChain.call(methodOutputComponent.method.call());
-                            }
-                        }
-                    },
-                    chainLinks + 1);
+            Callable<Void> sequentialCallChain = new Callable<>() {
+                @Override
+                public Void call() {
+                    synchronized (this) {
+                        nextMethodChain.call(method.call());
+                    }
+                    return null;
+                }
+            };
+            startShutInSequentialComponent(sequentialCallChain, chainLinks);
+        } else {
+            Callable<Void> parallelCallChain = () -> {
+                nextMethodChain.call(method.call());
+                return null;
+            };
+            startShutInParallelComponent(parallelCallChain);
         }
-        else{
-            new TickRunningStrategy(
-                    new AbstractComponent<>() {
-                        @Override
-                        public Collection<BoundedBuffer<Object>> getInputBuffers() {
-                            return Collections.singleton(getOutputPort().getBuffer());
-                        }
+    }
 
-                        @Override
-                        public Collection<BoundedBuffer<Object>> getOutputBuffers() {
-                            return Collections.emptyList();
-                        }
+    private void startShutInSequentialComponent(Callable<Void> sequentialCallChain, int chainLinks) {
+        new TickRunningStrategy(
+                createShutInComponent(sequentialCallChain),
+                chainLinks + 1);
+    }
 
-                        @Override
-                        protected void tick() {
-                            nextMethodChain.call(methodOutputComponent.method.call());
-                        }
-                    });
-        }
+    private void startShutInParallelComponent(Callable<Void> parallelCallChain) {
+        new TickRunningStrategy(createShutInComponent(parallelCallChain));
+    }
+
+    private AbstractComponent<V, Object> createShutInComponent(Callable<Void> callChain) {
+        return new AbstractComponent<>() {
+            @Override
+            public Collection<BoundedBuffer<V>> getInputBuffers() {
+                return Collections.singleton(OutputComponentChainLink.this.outputBuffer);
+            }
+
+            @Override
+            public Collection<BoundedBuffer<Object>> getOutputBuffers() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            protected void tick() {
+                try {
+                    callChain.call();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
     @Override
     Boolean isParallelisable() {
-        return methodOutputComponent.isParallelisable();
+        return method.isParallelisable();
     }
 
     @Override
@@ -99,13 +109,12 @@ public class OutputComponentChainLink<V> extends ComponentChainLink<Void, V> {
 
     @Override
     protected OutputPort getOutputPort() {
-        return methodOutputComponent.output;
+        return null;
     }
 
     public static <V> BufferChainLink<V> buildOutputBuffer(OutputCallable<V> method, int capacity, String name) {
         SimpleBuffer<V> outputBuffer = new SimpleBuffer<>(capacity, name);
-        MethodOutputComponent<V> component = new MethodOutputComponent<>(outputBuffer, method);
-        OutputComponentChainLink<V> componentChainLink = new OutputComponentChainLink<>(component);
+        OutputComponentChainLink<V> componentChainLink = new OutputComponentChainLink<>(method, outputBuffer);
         BufferChainLink<V> outputChainLink = new BufferChainLink<>(outputBuffer, componentChainLink);
         return outputChainLink;
     }
