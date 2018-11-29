d75f3f480a46c881fcc91ea00fdd5faeed51e7c5
Bram Oosterlee <bmoosterlee@gmail.com>
2018-11-29 22:17:20 +0100

    Clean up wrap method in PipeComponentChainLink

--- src/component/buffer/PipeComponentChainLink.java
+++ src/component/buffer/PipeComponentChainLink.java
@@ -17,146 +17,98 @@ public class PipeComponentChainLink<K, V> extends ComponentChainLink<K, V> {
 
     @Override
     protected void wrap() {
-        if(!isParallelisable()) {
-            if(previousComponentChainLink!=null) {
-                if (!previousComponentChainLink.isParallelisable()) {
-                    previousComponentChainLink.wrap(methodPipeComponent.method, getOutputPort().getBuffer(), 1);
-                } else {
-                    new TickRunningStrategy(methodPipeComponent, 1);
-
-                    previousComponentChainLink.wrap();
-                }
+        if(previousComponentChainLink!=null) {
+            if(isParallelisable()!=previousComponentChainLink.isParallelisable()){
+                startAutonomousComponent();
+                previousComponentChainLink.wrap();
+            } else if(!isParallelisable()) {
+                PipeCallable<K, V> sequentialMethod = new PipeCallable<>() {
+                    @Override
+                    public V call(K input) {
+                        synchronized (this) {
+                            return methodPipeComponent.method.call(input);
+                        }
+                    }
+                };
+                previousComponentChainLink.wrap(sequentialMethod, getOutputPort().getBuffer(), 1);
+            } else {
+                PipeCallable<K, V> parallelMethod = methodPipeComponent.method;
+                previousComponentChainLink.wrap(parallelMethod, getOutputPort().getBuffer(), 1);
             }
-            else{
-                new TickRunningStrategy(methodPipeComponent, 1);
-            }
+        } else {
+            startAutonomousComponent();
         }
-        else{
-            if(previousComponentChainLink!=null) {
-                if (previousComponentChainLink.isParallelisable()) {
-                    previousComponentChainLink.wrap(methodPipeComponent.method, getOutputPort().getBuffer(), 1);
-                } else {
-                    new TickRunningStrategy(methodPipeComponent);
+    }
 
-                    previousComponentChainLink.wrap();
-                }
-            }
-            else{
-                new TickRunningStrategy(methodPipeComponent);
-            }
-        }
+    private void startAutonomousComponent() {
+        new TickRunningStrategy(methodPipeComponent);
     }
 
     @Override
     protected void wrap(PipeCallable<V, ?> nextMethodChain, BoundedBuffer outputBuffer, int chainLinks) {
+        int newChainLinkCount = chainLinks + 1;
         if(!isParallelisable()) {
             PipeCallable<K, Object> sequentialCallChain = input -> {
                 synchronized (this) {
                     return nextMethodChain.call(methodPipeComponent.method.call(input));
                 }
             };
-
             if(previousComponentChainLink!=null) {
                 if (!previousComponentChainLink.isParallelisable()) {
-                    previousComponentChainLink.wrap(sequentialCallChain, outputBuffer, chainLinks + 1);
+                    previousComponentChainLink.wrap(sequentialCallChain, outputBuffer, newChainLinkCount);
                 } else {
-                    new TickRunningStrategy(
-                            new AbstractComponent<K, Object>() {
-
-                                private OutputPort outputPort = outputBuffer.createOutputPort();
-
-                                @Override
-                                protected Collection<BoundedBuffer<K>> getInputBuffers() {
-                                    return Collections.singleton(getInputPort().getBuffer());
-                                }
-
-                                @Override
-                                protected Collection<BoundedBuffer<Object>> getOutputBuffers() {
-                                    return Collections.singleton(outputBuffer);
-                                }
-
-                                @Override
-                                protected void tick() {
-                                    try {
-                                        outputPort.produce(
-                                                sequentialCallChain.call(
-                                                        getInputPort().consume()));
-                                    } catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }, chainLinks + 1);
-
+                    startChainedSequentialComponent(sequentialCallChain, outputBuffer, newChainLinkCount);
                     previousComponentChainLink.wrap();
                 }
+            } else {
+                startChainedSequentialComponent(sequentialCallChain, outputBuffer, newChainLinkCount);
             }
-            else{
-                new TickRunningStrategy(
-                        new AbstractComponent<K, Object>() {
-
-                            private OutputPort outputPort = outputBuffer.createOutputPort();
-
-                            @Override
-                            protected Collection<BoundedBuffer<K>> getInputBuffers() {
-                                return Collections.singleton(getInputPort().getBuffer());
-                            }
-
-                            @Override
-                            protected Collection<BoundedBuffer<Object>> getOutputBuffers() {
-                                return Collections.singleton(outputBuffer);
-                            }
-
-                            @Override
-                            protected void tick() {
-                                try {
-                                    outputPort.produce(
-                                            sequentialCallChain.call(
-                                                    getInputPort().consume()));
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }, chainLinks + 1);
-            }
-        }
-        else {
+        } else {
             PipeCallable<K, Object> parallelCallChain = input -> nextMethodChain.call(methodPipeComponent.method.call(input));
-
             if (previousComponentChainLink != null){
                 if (previousComponentChainLink.isParallelisable()) {
-                    previousComponentChainLink.wrap(parallelCallChain, outputBuffer, chainLinks + 1);
+                    previousComponentChainLink.wrap(parallelCallChain, outputBuffer, newChainLinkCount);
                 } else {
-                    new TickRunningStrategy(new AbstractComponent<K, Object>() {
-
-                        private OutputPort outputPort = outputBuffer.createOutputPort();
-
-                        @Override
-                        protected Collection<BoundedBuffer<K>> getInputBuffers() {
-                            return Collections.singleton(getInputPort().getBuffer());
-                        }
-
-                        @Override
-                        protected Collection<BoundedBuffer<Object>> getOutputBuffers() {
-                            return Collections.singleton(outputBuffer);
-                        }
-
-                        @Override
-                        protected void tick() {
-                            try {
-                                outputPort.produce(
-                                        parallelCallChain.call(
-                                                getInputPort().consume()));
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-
+                    startChainedParallelComponent(parallelCallChain, outputBuffer);
                     previousComponentChainLink.wrap();
                 }
+            } else {
+                startChainedParallelComponent(parallelCallChain, outputBuffer);
             }
-            else{
-                new TickRunningStrategy(new AbstractComponent<K, Object>() {
+        }
+    }
+
+    private void startChainedParallelComponent(PipeCallable<K, Object> parallelCallChain, BoundedBuffer outputBuffer) {
+        new TickRunningStrategy(new AbstractComponent<K, Object>() {
+
+            private OutputPort outputPort = outputBuffer.createOutputPort();
+
+            @Override
+            protected Collection<BoundedBuffer<K>> getInputBuffers() {
+                return Collections.singleton(getInputPort().getBuffer());
+            }
+
+            @Override
+            protected Collection<BoundedBuffer<Object>> getOutputBuffers() {
+                return Collections.singleton(outputBuffer);
+            }
+
+            @Override
+            protected void tick() {
+                try {
+                    outputPort.produce(
+                            parallelCallChain.call(
+                                    getInputPort().consume()));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void startChainedSequentialComponent(PipeCallable<K, Object> sequentialCallChain, BoundedBuffer outputBuffer, int chainLinks) {
+        new TickRunningStrategy(
+                new AbstractComponent<K, Object>() {
 
                     private OutputPort outputPort = outputBuffer.createOutputPort();
 
@@ -174,91 +126,74 @@ public class PipeComponentChainLink<K, V> extends ComponentChainLink<K, V> {
                     protected void tick() {
                         try {
                             outputPort.produce(
-                                    parallelCallChain.call(
+                                    sequentialCallChain.call(
                                             getInputPort().consume()));
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
-                });
-            }
-        }
+                }, chainLinks);
     }
 
     @Override
     protected void wrap(InputCallable<V> nextMethodChain, BoundedBuffer outputBuffer, int chainLinks) {
+        int newChainLinkCount = chainLinks + 1;
         if(!isParallelisable()) {
             InputCallable<K> sequentialCallChain = input -> {
                 synchronized (this) {
                     nextMethodChain.call(methodPipeComponent.method.call(input));
                 }
             };
-
             if (previousComponentChainLink != null){
                 if (!previousComponentChainLink.isParallelisable()) {
-                    previousComponentChainLink.wrap(sequentialCallChain, outputBuffer, chainLinks + 1);
+                    previousComponentChainLink.wrap(sequentialCallChain, outputBuffer, newChainLinkCount);
                 } else {
-                    new TickRunningStrategy(new AbstractInputComponent<>(getInputPort()) {
-                        @Override
-                        protected void tick() {
-                            try {
-                                sequentialCallChain.call(getInputPort().consume());
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }, chainLinks + 1);
-
+                    startChainedSequentialComponent(sequentialCallChain, newChainLinkCount);
                     previousComponentChainLink.wrap();
                 }
-            }
-            else{
-                new TickRunningStrategy(new AbstractInputComponent<>(getInputPort()) {
-                    @Override
-                    protected void tick() {
-                        try {
-                            sequentialCallChain.call(getInputPort().consume());
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, chainLinks + 1);
+            } else {
+                startChainedSequentialComponent(sequentialCallChain, newChainLinkCount);
             }
         }
         else{
             InputCallable<K> parallelCallChain = input -> nextMethodChain.call(methodPipeComponent.method.call(input));
-
             if(previousComponentChainLink!=null) {
                 if (previousComponentChainLink.isParallelisable()) {
-                    previousComponentChainLink.wrap(parallelCallChain, outputBuffer, chainLinks + 1);
+                    previousComponentChainLink.wrap(parallelCallChain, outputBuffer, newChainLinkCount);
                 } else {
-                    new TickRunningStrategy(new AbstractInputComponent<>(getInputPort()) {
-                        @Override
-                        protected void tick() {
-                            try {
-                                parallelCallChain.call(getInputPort().consume());
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-
+                    startChainedParallelComponent(parallelCallChain);
                     previousComponentChainLink.wrap();
                 }
-            }
-            else{
-                new TickRunningStrategy(new AbstractInputComponent<>(getInputPort()) {
-                    @Override
-                    protected void tick() {
-                        try {
-                            parallelCallChain.call(getInputPort().consume());
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+            } else{
+                startChainedParallelComponent(parallelCallChain);
             }
         }
+    }
+
+    private void startChainedParallelComponent(InputCallable<K> parallelCallChain) {
+        new TickRunningStrategy(new AbstractInputComponent<>(getInputPort()) {
+            @Override
+            protected void tick() {
+                try {
+                    parallelCallChain.call(getInputPort().consume());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void startChainedSequentialComponent(InputCallable<K> sequentialCallChain, int newChainLinkCount) {
+        new TickRunningStrategy(new AbstractInputComponent<>(getInputPort()) {
+            @Override
+            protected void tick() {
+                try {
+                    sequentialCallChain.call(getInputPort().consume());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, newChainLinkCount);
     }
 
     @Override
