4228a714f54178fd306e60cbcf4ae1f6b81f4681
Bram Oosterlee <bmoosterlee@gmail.com>
2018-12-01 17:50:33 +0100

    Implement Mapper class which consumes key value pairs and produces to a map of buffers

---
+++ src/component/buffer/Mapper.java
@@ -0,0 +1,52 @@
+package component.buffer;
+
+import java.util.*;
+import java.util.AbstractMap.SimpleImmutableEntry;
+
+public class Mapper<K, V> extends AbstractComponent<SimpleImmutableEntry<K, V>, V> {
+    private InputPort<SimpleImmutableEntry<K, V>> inputPort;
+    private final Map<K, OutputPort<V>> outputPortMap;
+
+    private Mapper(BoundedBuffer<SimpleImmutableEntry<K, V>> inputBuffer, Map<K, BoundedBuffer<V>> outputBufferMap) {
+        inputPort = inputBuffer.createInputPort();
+        outputPortMap = new HashMap<>();
+        for (K index : outputBufferMap.keySet()) {
+            outputPortMap.put(index, outputBufferMap.get(index).createOutputPort());
+        }
+    }
+
+    @Override
+    protected void tick() {
+        try {
+            SimpleImmutableEntry<K, V> consumed = inputPort.consume();
+            outputPortMap.get(consumed.getKey()).produce(consumed.getValue());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Collection<BoundedBuffer<SimpleImmutableEntry<K, V>>> getInputBuffers() {
+        return Collections.singleton(inputPort.getBuffer());
+    }
+
+    @Override
+    protected Collection<BoundedBuffer<V>> getOutputBuffers() {
+        HashSet<BoundedBuffer<V>> outputBuffers = new HashSet<>();
+        for(OutputPort<V> outputPort : outputPortMap.values()){
+            outputBuffers.add(outputPort.getBuffer());
+        }
+        return outputBuffers;
+    }
+
+    public static <K, V> Map<K, BoundedBuffer<V>> toBufferMap(BoundedBuffer<SimpleImmutableEntry<K, V>> inputBuffer, Collection<K> range){
+        Map<K, BoundedBuffer<V>> outputBufferMap = new HashMap<>();
+        for (K index : range) {
+            outputBufferMap.put(index, new SimpleBuffer<>(1,"map bucket"));
+        }
+
+        new TickRunningStrategy(new Mapper<>(inputBuffer, outputBufferMap));
+
+        return outputBufferMap;
+    }
+}
