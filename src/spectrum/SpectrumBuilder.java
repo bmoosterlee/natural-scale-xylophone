ba2267ac3edcc524318ae6ef94a6e0a19de92052
Bram Oosterlee <bmoosterlee@gmail.com>
2018-12-01 17:51:03 +0100

    Refactor SpectrumBuilder to use buffer chaining and Mapper class

--- src/spectrum/SpectrumBuilder.java
+++ src/spectrum/SpectrumBuilder.java
@@ -1,6 +1,7 @@
 package spectrum;
 
 import component.Pulse;
+import component.Separator;
 import component.TimedConsumer;
 import component.buffer.*;
 import frequency.Frequency;
@@ -14,10 +15,12 @@ import spectrum.harmonics.HarmonicCalculator;
 
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SpectrumBuilder {
 
-    public static SimpleImmutableEntry<BoundedBuffer<Buckets>, BoundedBuffer<Buckets>> buildComponent(BoundedBuffer<Pulse> frameTickBuffer, BoundedBuffer<VolumeState> inputBuffer, SpectrumWindow spectrumWindow, int width) {
+    public static SimpleImmutableEntry<BoundedBuffer<Buckets>, BoundedBuffer<Buckets>> buildComponent(BoundedBuffer<Pulse> frameTickBuffer, BoundedBuffer<VolumeState> inputBuffer, SpectrumWindow spectrumWindow) {
         LinkedList<BoundedBuffer<Pulse>> tickBroadcast = new LinkedList<>(frameTickBuffer.broadcast(2, "spectrum builder tick - broadcast"));
         LinkedList<BoundedBuffer<VolumeState>> volumeBroadcast =
             new LinkedList<>(
@@ -25,56 +28,39 @@ public class SpectrumBuilder {
                 .performMethod(TimedConsumer.consumeFrom(inputBuffer), "consume from input buffer")
                 .broadcast(2, "Spectrum builder volume - broadcast"));
 
-        BoundedBuffer<Buckets> noteOutputBuffer =
-            volumeBroadcast.poll()
-            .performMethod(input -> new Buckets(input, spectrumWindow), "build volume state to buckets");
-
-
-        Map<Integer, OutputPort<AtomicBucket>> harmonicsOutput = new HashMap<>();
-        for (Integer i = 0; i < width; i++) {
-            harmonicsOutput.put(i, new OutputPort<>("harmonic bucket"));
-        }
-
         BufferChainLink<Iterator<Map.Entry<Harmonic, Double>>> harmonicsIteratorBuffer = volumeBroadcast.poll()
                 .performMethod(HarmonicCalculator.calculateHarmonics(100), "calculate harmonics iterator");
 
-        harmonicsIteratorBuffer.performInputMethod(input -> {
-                    while (harmonicsIteratorBuffer.isEmpty()) {
-                        if (!update(input, harmonicsOutput, spectrumWindow)) break;
+
+        return new SimpleImmutableEntry<>(
+                volumeBroadcast.poll()
+                .performMethod(input -> new Buckets(input, spectrumWindow), "build volume state to buckets"),
+                tickBroadcast.poll()
+                .connectTo(
+                        BuffersToBuckets.buildPipe(
+                            Mapper.toBufferMap(
+                                calculateHarmonicsContinuously(harmonicsIteratorBuffer)
+                                .connectTo((PipeCallable<BoundedBuffer<Collection<Map.Entry<Harmonic, Double>>>, BoundedBuffer<Map.Entry<Harmonic, Double>>>) Separator::separate)
+                                .performMethod(harmonicWithVolume -> {
+                                    Frequency frequency = harmonicWithVolume.getKey().getHarmonicFrequency();
+                                    return new SimpleImmutableEntry<>(frequency, harmonicWithVolume.getValue());
+                                })
+                                .performMethod(input -> new SimpleImmutableEntry<>(input.getKey(), new AtomicBucket(input.getKey(), input.getValue())))
+                                .performMethod(input -> new SimpleImmutableEntry<>(spectrumWindow.getX(input.getKey()), input.getValue()))
+                                .connectTo(spectrumWindow.buildInBoundsFilterPipe()),
+                            IntStream.range(0, spectrumWindow.width).boxed().collect(Collectors.toSet()))))
+                .connectTo(PrecalculatedBucketHistoryComponent.buildPipe(200)));
+    }
+
+    private static BufferChainLink<Collection<Map.Entry<Harmonic, Double>>> calculateHarmonicsContinuously(BufferChainLink<Iterator<Map.Entry<Harmonic, Double>>> harmonicsIteratorBuffer) {
+        return harmonicsIteratorBuffer.performMethod(
+                harmonicsIterator -> {
+                    HashSet<Map.Entry<Harmonic, Double>> newHarmonics = new HashSet<>();
+                    while (harmonicsIteratorBuffer.isEmpty() && harmonicsIterator.hasNext()) {
+                        newHarmonics.add(harmonicsIterator.next());
                     }
+                    return newHarmonics;
                 });
-
-        Map<Integer, BoundedBuffer<AtomicBucket>> harmonicsMap = new HashMap<>();
-        for (Integer index : harmonicsOutput.keySet()) {
-            harmonicsMap.put(index, harmonicsOutput.get(index).getBuffer().resize(1000));
-        }
-
-        BoundedBuffer<Buckets> harmonicsOutputBuffer =
-            tickBroadcast.poll()
-            .connectTo(BuffersToBuckets.buildPipe(harmonicsMap))
-            .connectTo(PrecalculatedBucketHistoryComponent.buildPipe(200));
-
-        return new SimpleImmutableEntry<>(noteOutputBuffer, harmonicsOutputBuffer);
     }
 
-
-    //return true when harmonicHierarchy has been depleted.
-    private static boolean update(Iterator<Map.Entry<Harmonic, Double>> harmonicHierarchyIterator, Map<Integer, OutputPort<AtomicBucket>> harmonicsOutput, SpectrumWindow spectrumWindow) {
-        try {
-            Map.Entry<Harmonic, Double> harmonicVolume = harmonicHierarchyIterator.next();
-            Frequency frequency = harmonicVolume.getKey().getHarmonicFrequency();
-
-            AtomicBucket newBucket = new AtomicBucket(frequency, harmonicVolume.getValue());
-            harmonicsOutput.get(spectrumWindow.getX(frequency)).produce(newBucket);
-
-        } catch (NoSuchElementException e) {
-            return false;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            return false;
-        } catch (NullPointerException ignored) {
-            //harmonic is out of bounds during the call to harmonicsOutput.get
-        }
-        return true;
-    }
 }
