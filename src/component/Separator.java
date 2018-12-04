cbdacebb583ffe290658302893a20ce168218b03
Bram Oosterlee <bmoosterlee@gmail.com>
2018-12-04 21:59:42 +0100

    Extract strategies from Separator, implement packet conserving strategy for use within package

--- src/component/Separator.java
+++ src/component/Separator.java
@@ -3,57 +3,56 @@ package component;
 import component.buffer.*;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 
-public class Separator<T, L extends Collection<T>, A extends Packet<T>, B extends Packet<L>> extends AbstractComponent<L, T, B, A> {
-
-    private final InputPort<L, B> inputPort;
-    private final OutputPort<T, A> outputPort;
-
-    public Separator(BoundedBuffer<L, B> inputBuffer, SimpleBuffer<T, A> outputBuffer) {
-        inputPort = inputBuffer.createInputPort();
-        outputPort = outputBuffer.createOutputPort();
-    }
-
-    protected void tick() {
-        try {
-            B input = inputPort.consume();
-
-            Collection<A> col = new HashSet<>();
-            for(T element : input.unwrap()){
-                col.add(input.transform(in -> element));
-            }
-
-            for(A element : col) {
-                outputPort.produce(element);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+public class Separator{
 
     public static <T, L extends Collection<T>, A extends Packet<T>, B extends Packet<L>> PipeCallable<BoundedBuffer<L, B>, BoundedBuffer<T, A>> buildPipe(){
+        return separateInternal(Separator::separate);
+    }
+
+    static <T, L extends Collection<A>, A extends Packet<T>, B extends Packet<L>> PipeCallable<BoundedBuffer<L, B>, BoundedBuffer<T, A>> buildPacketsPipe(){
+        return separateInternal(Separator::separatePackets);
+    }
+
+    private static <T, L extends Collection<?>, A extends Packet<T>, B extends Packet<L>> PipeCallable<BoundedBuffer<L, B>, BoundedBuffer<T, A>> separateInternal(final PipeCallable<B, Collection<A>> separationStrategy) {
         return inputBuffer -> {
             SimpleBuffer<T, A> outputBuffer = new SimpleBuffer<>(1, "separator - output");
-            new TickRunningStrategy(new Separator<>(inputBuffer, outputBuffer));
+            new TickRunningStrategy(new AbstractPipeComponent<>(inputBuffer.createInputPort(), outputBuffer.createOutputPort()) {
+                @Override
+                protected void tick() {
+                    try {
+                        B consumed = input.consume();
+
+                        Collection<A> col = separationStrategy.call(consumed);
+
+                        for (A element : col) {
+                            output.produce(element);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public Boolean isParallelisable(){
+                    return false;
+                }
+
+            });
             return outputBuffer;
         };
     }
 
-    @Override
-    protected Collection<BoundedBuffer<L, B>> getInputBuffers() {
-        return Collections.singleton(inputPort.getBuffer());
+    private static <T, L extends Collection<T>, A extends Packet<T>, B extends Packet<L>> Collection<A> separate(B consumed) {
+        Collection<A> col = new HashSet<>();
+        for (T element : consumed.unwrap()) {
+            col.add(consumed.transform(in -> element));
+        }
+        return col;
     }
 
-    @Override
-    protected Collection<BoundedBuffer<T, A>> getOutputBuffers() {
-        return Collections.singleton(outputPort.getBuffer());
+    private static <T, L extends Collection<A>, A extends Packet<T>, B extends Packet<L>> Collection<A> separatePackets(B consumed) {
+        return new HashSet<>(consumed.unwrap());
     }
-
-    @Override
-    public Boolean isParallelisable(){
-        return false;
-    }
-
 }
