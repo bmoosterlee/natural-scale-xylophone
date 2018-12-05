2d2754484b847942fcabf5be19ee01c4e176e14b
Bram Oosterlee <bmoosterlee@gmail.com>
2018-12-05 22:15:08 +0100

    Implement continuous defragmentation while waiting for input in Orderer

--- src/component/orderer/Orderer.java
+++ src/component/orderer/Orderer.java
@@ -2,16 +2,17 @@ package component.orderer;
 
 import component.buffer.*;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Orderer<T> extends AbstractPipeComponent<T, T, OrderStampedPacket<T>, OrderStampedPacket<T>> {
     private final PriorityQueue<OrderStampedPacket<T>> backlog;
+    private final PriorityQueue<LinkedList<OrderStampedPacket<T>>> defragmentedBacklog;
     private OrderStampedPacket<T> index;
 
     public Orderer(BoundedBuffer<T, OrderStampedPacket<T>> input, BoundedBuffer<T, OrderStampedPacket<T>> output) {
         super(input.createInputPort(), output.createOutputPort());
         backlog = new PriorityQueue<>();
+        defragmentedBacklog = new PriorityQueue<>(Comparator.comparing(LinkedList::peek));
     }
 
     @Override
@@ -21,12 +22,29 @@ public class Orderer<T> extends AbstractPipeComponent<T, T, OrderStampedPacket<T
 
             backlog.addAll(newPackets);
             if (index != null) {
-                clearBacklogContinuously();
+                clearBacklog();
+                defragmentContinuously();
             } else {
                 addPacketsBeforeFindingFirstStamp();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void clearBacklog() throws InterruptedException {
+        while ((!defragmentedBacklog.isEmpty() && index.successor(defragmentedBacklog.peek().peek())) || (!backlog.isEmpty() && index.successor(backlog.peek()))) {
+            if (!defragmentedBacklog.isEmpty() && index.successor(defragmentedBacklog.peek().peek())) {
+                LinkedList<OrderStampedPacket<T>> nextFragment = defragmentedBacklog.poll();
+                index = nextFragment.peekLast();
+                for (OrderStampedPacket<T> packet : nextFragment) {
+                    output.produce(packet);
+                }
+            }
+            if (!backlog.isEmpty() && index.successor(backlog.peek())) {
+                index = backlog.poll();
+                output.produce(index);
+            }
         }
     }
 
@@ -42,10 +60,27 @@ public class Orderer<T> extends AbstractPipeComponent<T, T, OrderStampedPacket<T
         }
     }
 
-    private void clearBacklogContinuously() throws InterruptedException {
-        while (!backlog.isEmpty() && index.successor(backlog.peek())) {
-            index = backlog.poll();
-            output.produce(index);
+    private void defragmentContinuously() {
+        while(input.isEmpty() && !backlog.isEmpty()){
+            LinkedList<OrderStampedPacket<T>> newEntry = new LinkedList<>(Collections.singletonList(backlog.poll()));
+            HashSet<LinkedList<OrderStampedPacket<T>>> candidatesLowerThanEntry = new HashSet<>();
+            while(!defragmentedBacklog.isEmpty()){
+                LinkedList<OrderStampedPacket<T>> candidate = defragmentedBacklog.poll();
+                if(newEntry.peekLast().successor(candidate.peek())){
+                    newEntry.addAll(candidate);
+                    break;
+                } else if(candidate.peekLast().successor(newEntry.peek())){
+                    candidate.addAll(newEntry);
+                    newEntry = candidate;
+                } else if(newEntry.peekLast().compareTo(candidate.peek()) < 0){
+                    defragmentedBacklog.add(candidate);
+                    break;
+                } else {
+                    candidatesLowerThanEntry.add(candidate);
+                }
+            }
+            defragmentedBacklog.addAll(candidatesLowerThanEntry);
+            defragmentedBacklog.add(newEntry);
         }
     }
 
