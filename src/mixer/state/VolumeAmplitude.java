0be3dc651d7ddcf9eee4a6b908a6080ba24a31f3
Bram Oosterlee <bmoosterlee@gmail.com>
2018-12-06 23:29:51 +0100

    Clean up code

--- src/mixer/state/VolumeAmplitude.java
+++ src/mixer/state/VolumeAmplitude.java
@@ -1,29 +1,12 @@
 package mixer.state;
 
-import java.util.Collection;
-
 public class VolumeAmplitude {
-    public final Double volume;
-    public final Double amplitude;
+    private final Double volume;
+    private final Double amplitude;
 
-    public VolumeAmplitude(Double volume, Double amplitude) {
+    VolumeAmplitude(Double volume, Double amplitude) {
         this.volume = volume;
-        if(amplitude==null){
-            throw new NullPointerException();
-        }
         this.amplitude = amplitude;
-    }
-
-    public static VolumeAmplitude sum(Collection<VolumeAmplitude> volumeAmplitudeCollection) {
-        VolumeAmplitude total = new VolumeAmplitude(0., 0.);
-        for(VolumeAmplitude element : volumeAmplitudeCollection){
-            total = total.add(element.volume);
-        }
-        return total;
-    }
-
-    public VolumeAmplitude add(Double other) {
-        return new VolumeAmplitude(volume + other, amplitude);
     }
 
     public double getValue() {
