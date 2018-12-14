8c60ca88d2e329411fa5e476447522ccf4eb66de
Bram Oosterlee <bmoosterlee@gmail.com>
2018-12-14 21:35:31 +0100

    Report traffic analysis once per second

--- src/component/buffer/TrafficAnalyzer.java
+++ src/component/buffer/TrafficAnalyzer.java
@@ -110,7 +110,7 @@ public class TrafficAnalyzer {
                 }
 
                 try {
-                    Thread.sleep(10000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
